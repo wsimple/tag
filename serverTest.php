e75fe543d6a1acefb0556e8ccd93bc093c1df34b
Adrian esqueda <aesqueda14@gmail.com>
2014-07-01 19:41:49 -0430

    hmiharbi pushing

--- serverTest.php
+++ serverTest.php
@@ -3,6 +3,7 @@
   // ini_set('display_errors', 1);
   //netbeans testing
   //second push
+  //hmiharbi activando la jugada
 
   error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
   error_reporting(E_ALL);
