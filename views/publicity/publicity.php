4f759fd78d1884b4a3228860884cdb1bd9c9c70f
Adrian esqueda <aesqueda14@gmail.com>
2014-07-08 11:12:05 -0430

    ajustes publicidad
    Former-commit-id: 7f4cf766a1d407a6d67a72d64b5104f09e441831

--- views/publicity/publicity.php
+++ views/publicity/publicity.php
@@ -35,7 +35,7 @@
 			<a href="<?=$publicity['link']?>" target="_blank" onclick="showPublicityWb('<?=$publicity['id']?>')" onfocus="this.blur()">
 				<div id="imgPublicity">
 					<?php
-						if (file_exists('img/publicity/a'.$publicity['picture'])) {
+						if (file_exists('img/publicity/'.$publicity['picture'])) {
 							$img = 'includes/imagen.php?ancho=90&tipo=3&img='.FILESERVER.'img/publicity/'.$publicity['picture'];
 						}else{
 							$img = 'img/publicity/publicity_nofile.png';
@@ -81,7 +81,7 @@
 				<a href="<?=$publicity['link']?>" target="_blank" onclick="showPublicityWb('<?=$publicity['id']?>')" onfocus="this.blur()">
 					<div id="imgPublicity">
 						<?php
-							if (file_exists('img/publicity/a'.$publicity['picture'])) {
+							if (file_exists('img/publicity/'.$publicity['picture'])) {
 								$img = 'includes/imagen.php?ancho=90&tipo=3&img='.FILESERVER.'img/publicity/'.$publicity['picture'];
 							}else{
 								$img = 'img/publicity/publicity_nofile.png';
