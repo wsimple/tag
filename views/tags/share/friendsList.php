5c4de6e5acad9c1bfe82b263f9bb3dcc7a12a313
ramonJR <maxtri@hotmail.com>
2014-07-11 10:56:27 -0430

    nuevo texto en invitar amigos, solucionado bugs arreglado maquetacion share tag
    Former-commit-id: 70c693e5e4c7a086cf98e169931e1469355e1004

--- views/tags/share/friendsList.php
+++ views/tags/share/friendsList.php
@@ -5,20 +5,7 @@
 	include('../../../class/wconecta.class.php');
 	include('../../../includes/languages.config.php');
 ?>
-<script type="text/javascript">
-	$(document).ready(function(){		
-		$("#seek_friends").focus(function() {
-			if ($.trim($("#seek_friends").val())=="<?=FRIENDS_FINDFRIEND_SEARCH?>")
-				$("#seek_friends").val("");
-		});
-		$("#seek_friends").blur(function() {
-			if ($.trim($("#seek_friends").val())=="")
-				$("#seek_friends").val("<?=FRIENDS_FINDFRIEND_SEARCH?>");
-		});
-//		$("button, input:submit, input:reset, input:button").button();
-	});
-</script>
-<div class="table_frm_new_group" style="margin-top:-0px; padding:5px; height:auto; width:580px">
+<div style="margin-top:-0px; padding:5px; height:auto; width:580px">
 <table width="99%" border="0" cellspacing="0" cellpadding="0" class="table_frm_new_group" style="margin-top:-0px; border:1px solid #fff;">
 	<?php
 		unset($_SESSION['ws-tags']['lstUsrBrowser']);
@@ -35,3 +22,16 @@
 	?>
 </table>
 </div>
+<script type="text/javascript">
+	$(document).ready(function(){		
+		$("#seek_friends").focus(function() {
+			if ($.trim($("#seek_friends").val())=="<?=FRIENDS_FINDFRIEND_SEARCH?>")
+				$("#seek_friends").val("");
+		});
+		$("#seek_friends").blur(function() {
+			if ($.trim($("#seek_friends").val())=="")
+				$("#seek_friends").val("<?=FRIENDS_FINDFRIEND_SEARCH?>");
+		});
+//		$("button, input:submit, input:reset, input:button").button();
+	});
+</script>
--- views/users/browser/grid.view.php
+++ views/users/browser/grid.view.php
@@ -1,26 +1,17 @@
 <?php
+	$fieldWhereDatail = 'id_friend';
+	$title = USERS_BROWSERFRIENDSTITLEOP1;
+	$labelTxtBroswer = USERS_BROWSERFRIENDSLABELTXT1;
 	switch ($brower_type){
 		case 1:
-			$lstUsrs = $GLOBALS['cn']->query('
-				SELECT id_user
-				FROM users_groups
-				WHERE md5(id_group) = "'.$_GET['grp'].'" AND id_user != "'.$_SESSION['ws-tags']['ws-user']['id'].'" AND status=1
-				GROUP BY id_user
-			');
-
-			$arrayMembersGroup = mysqlFetchAssocToArray($lstUsrs, 'id_user'); //array de miembros del grupo
-
-			//_imprimir($arrayMembersGroup);
-			if (count($arrayMembersGroup)>0){
+			$lstUsrs=CON::getArray('SELECT id_user FROM users_groups
+									WHERE md5(id_group)=? AND id_user !=? AND (status=1 OR status=2)
+									GROUP BY id_user',array(intToMd5($_GET['grp']),$_SESSION['ws-tags']['ws-user']['id']));
+			if (count($lstUsrs)>0){
 				$idsNotIn = '';
-				foreach ($arrayMembersGroup as $id_menber) $idsNotIn .= "'".$id_menber."',";
+				for ($i=0;$i<count($lstUsrs);$i++) $idsNotIn .= "'".$lstUsrs[$i][0]."',";
 				$friends = view_friends('','', " AND f.id_friend NOT IN (".rtrim($idsNotIn,',').") ");
 			}else{ $friends = view_friends(); }
-
-			$title = USERS_BROWSERFRIENDSTITLEOP1;
-			$labelTxtBroswer = USERS_BROWSERFRIENDSLABELTXT1;
-			$fieldWhereDatail = 'id_friend';
-
 		break;
 		case 2:
 			//echo $_GET['mails'];
@@ -35,60 +26,11 @@
 					$arrayMembersGroup[] = $_email;
 				}else{ $arrayMembersGroup[] = $_email; }	
 			}
+			$found_mails=array();
 			$friends = view_friends();
-			$title = USERS_BROWSERFRIENDSTITLEOP1;
-			$labelTxtBroswer = USERS_BROWSERFRIENDSLABELTXT1;
-			$fieldWhereDatail = 'id_friend';
 		break;
 	}
 ?>
-<script type="text/javascript">
-	$(document).ready(function(){
-		$("#seekUserLstSeemytag")
-		.focus(function() {
-			if ($.trim($(this).val())=="<?=$labelTxtBroswer?>")
-				$(this).val("");
-		})
-		.blur(function() {
-			if ($.trim($(this).val())=="")
-				$(this).val("<?=$labelTxtBroswer?>");
-		})
-		.keyup(function() {
-			var txt=$.trim($(this).val());
-			var like = new RegExp(txt,"i");
-
-			$("#boxUserLstSeemytag li").each(function(i, li){
-				if( txt=="" || like.test($("strong",li).text()) || like.test($(":checkbox",li).val()) )
-					$(li).show();
-				else
-					$(li).hide();
-			});
-
-			/*("#loaderUserLstSeemytag").html('<img src="css/smt/loader.gif" width="12" height="12" border="0" />');
-			$.ajax({
-				type: "GET",
-				url: "views/users/browser/ajax.view.php?brower_type=<?=$brower_type?>&like="+like,
-				dataType: "html",
-				success: function (data)
-				{
-					$("#boxUserLstSeemytag").html(data);
-				}
-			});
-
-			$("#loaderUserLstSeemytag").html("");
-			/**/
-		});
-
-		$("#hrefNoneBoxLstSeemytag").click(function(){
-			$("#contentLstUsersSeemytag :checkbox").attr("checked", false).change();
-		});
-
-		$("#hrefAllBoxLstSeemytag").click(function(){
-			$("#contentLstUsersSeemytag :checkbox").attr("checked", true).change();
-		});
-	});
-</script>
-
 <tr>
 	<td width="18" style="padding-bottom:5px"><img src="css/smt/menu_left/friends.png" width="16" height="16" border="0" /></td>
 	<td style="padding-bottom:5px; font-size:12px">
@@ -108,80 +50,97 @@
 
 <tr>
 	<td colspan="2" valign="top">
-	<div id="contentLstUsersSeemytag">
-		<ul id="boxUserLstSeemytag">
-			<?php
-				$found_mails=array();
-				while ($friend = mysql_fetch_assoc($friends)){
-					$query = $GLOBALS['cn']->query('
-						SELECT	u.username AS username,
-								(SELECT a.name FROM countries a WHERE a.id=u.country) AS pais,
-								u.followers_count AS followers,
-								u.friends_count AS friends
-						FROM users u
-						WHERE u.id = "'.$friend[$fieldWhereDatail].'"
-					');
-					$array = mysql_fetch_assoc($query);
-					$id_layer = md5($friend['id_friend']);
-					$val_layer = ($brower_type==2) ? $friend['email'] : $id_layer;
-			?>
-			<li id="liBoxUserLstSeemytag_<?=$id_layer?>">
-				<div>
-					<strong style="color:#E78F08"><?=$friend['name_user']?></strong><br/>
-					<?php if($brower_type==2){ ?>
-						<?=SIGNUP_LBLEMAIL?>:&nbsp;<span style="color:#E78F08"><?=$val_layer?></span><br>
-					<?php }
-					if(trim($array['username'])!='' && $brower_type==1){ ?>
-						<?=USERS_BROWSERFRIENDSLABELEXTERNALPROFILE?>:&nbsp;<span><a href="<?=DOMINIO.$array['username']?>" onFocus="this.blur();" target="_blank"><?=DOMINIO.$array['username']?></a></span><br/>
-					<?php }
-					if(trim($array['pais'])!=''){ ?>
-						<?=USERS_BROWSERFRIENDSLABELCOUNTRY?>:&nbsp;<span><?=$array['pais']?></span><br/>
-					<?php } ?>
-					<?=USERS_BROWSERFRIENDSLABELFRIENDS?>(<?=$array['friends']?>),&nbsp;<?=USERS_BROWSERFRIENDSLABELADMIRERS?>(<?=$array['followers']?>)
-				</div>
-				<?php $photoUser=FILESERVER.getUserPicture($friend['code_friend'].'/'.$friend['photo_friend']); ?>
-				<img src="<?=$photoUser?>" border="0"  width="60" height="60" />
-				<p>
-					<?php
-						$checked = '';
-						if ($brower_type==1 && @in_array($friend['id_friend'], $arrayMembersGroup)) $checked = 'checked="checked"';
-						if ($brower_type==2){
-							if(@in_array($friend['email'], $arrayMembersGroup)){
-								$checked = 'checked="checked"';
-								$found_mails[]=$friend['email'];
+		<div id="contentLstUsersSeemytag">
+			<ul id="boxUserLstSeemytag">
+				<?php
+					while ($friend = mysql_fetch_assoc($friends)){
+						$query = $GLOBALS['cn']->query('
+							SELECT	u.username AS username,
+									(SELECT a.name FROM countries a WHERE a.id=u.country) AS pais,
+									u.followers_count AS followers,
+									u.friends_count AS friends
+							FROM users u
+							WHERE u.id = "'.$friend[$fieldWhereDatail].'"
+						');
+						$array = mysql_fetch_assoc($query);
+						$id_layer = md5($friend['id_friend']);
+						$val_layer = ($brower_type==2) ? $friend['email'] : $id_layer;
+				?>
+				<li id="liBoxUserLstSeemytag_<?=$id_layer?>">
+					<div>
+						<strong style="color:#E78F08"><?=$friend['name_user']?></strong><br/>
+						<?php if($brower_type==2){ ?>
+							<?=SIGNUP_LBLEMAIL?>:&nbsp;<span style="color:#E78F08"><?=$val_layer?></span><br>
+						<?php }
+						if(trim($array['username'])!='' && $brower_type==1){ ?>
+							<?=USERS_BROWSERFRIENDSLABELEXTERNALPROFILE?>:&nbsp;<span><a href="<?=DOMINIO.$array['username']?>" onFocus="this.blur();" target="_blank"><?=DOMINIO.$array['username']?></a></span><br/>
+						<?php }
+						if(trim($array['pais'])!=''){ ?>
+							<?=USERS_BROWSERFRIENDSLABELCOUNTRY?>:&nbsp;<span><?=$array['pais']?></span><br/>
+						<?php } ?>
+						<?=USERS_BROWSERFRIENDSLABELFRIENDS?>(<?=$array['friends']?>),&nbsp;<?=USERS_BROWSERFRIENDSLABELADMIRERS?>(<?=$array['followers']?>)
+					</div>
+					<?php $photoUser=FILESERVER.getUserPicture($friend['code_friend'].'/'.$friend['photo_friend']); ?>
+					<img src="<?=$photoUser?>" border="0"  width="60" height="60" />
+					<p>
+						<?php
+							$checked = '';
+							if ($brower_type==1 && @in_array($friend['id_friend'], $arrayMembersGroup)) $checked = 'checked="checked"';
+							if ($brower_type==2){
+								if(@in_array($friend['email'], $arrayMembersGroup)){
+									$checked = 'checked="checked"';
+									$found_mails[]=$friend['email'];
+								}
 							}
-						}
-					?>
-					<input type="checkbox" <?=$checked?> id="chkLstUsersBroswer_<?=$id_layer?>"  name="chkLstUsersBroswer_<?=$id_layer?>" value="<?=$id_layer?>" />
-					<label for="chkLstUsersBroswer_<?=$id_layer?>"></label>
-				</p>
-			</li>
-			<script type="text/javascript">
-				$(document).ready(function(){
-					$("#chkLstUsersBroswer_<?=$id_layer?>").button()
-					$("#chkLstUsersBroswer_<?=$id_layer?>").change(function() {
-						var li=$(this).parent().parent();// checkbox < p < li
-						if ($(this).is(':checked')){
-							li.css("background-color","FFFEE0");
-							li.find("label span").text("<?=$share?TAGS_OPTIONSHARENO:USERS_BROWSERFRIENDSLABELBTNUNINVITE?>");
-						}else{
-							li.css("background-color","FFF");
-							li.find("label span").text("<?=$share?TAGS_OPTIONSHARE:USERS_BROWSERFRIENDSLABELBTNINVITE?>");
-						}
-					}).change();
-				});
-			</script>
-			<?php }//while friends ?>
-		</ul>
-		<?php if($brower_type==2){ ?>
-			<input name="extramails" id="extramails" type="hidden" value="<?=  implode(',', array_diff($arrayMembersGroup, $found_mails))?>" />
-		<?php } ?>
-	</div>
-	<?php if($friend_total <= 0){?>
-	<div>
-		<?=USER_FINDMOREFRIENDS?>&nbsp;
-		<a href="javascript:void(0);" onfocus="this.blur();"  onclick="onClickFriends('friends'); $('#viewsGroups').dialog('destroy');"><?=USER_FINDHERE?></a>
-	</div>
-	<?php }?>
+						?>
+						<input type="checkbox" <?=$checked?> id="chkLstUsersBroswer_<?=$id_layer?>"  name="chkLstUsersBroswer_<?=$id_layer?>" value="<?=$id_layer?>" />
+						<label for="chkLstUsersBroswer_<?=$id_layer?>"></label>
+					</p>
+				</li>
+				<?php }//while friends ?>
+			</ul>
+			<?php if($brower_type==2){ ?>
+				<input name="extramails" id="extramails" type="hidden" value="<?=  implode(',', array_diff($arrayMembersGroup, $found_mails))?>" />
+			<?php } ?>
+		</div>
+		<?php if($friend_total <= 0){?>
+		<div>
+			<?=USER_FINDMOREFRIENDS?>&nbsp;<a href="tb.php/friends?sc=2" ><?=USER_FINDHERE?></a>
+		</div>
+		<?php }?>
 	</td>
 </tr>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$("#seekUserLstSeemytag").focus(function() {
+			if ($.trim($(this).val())=="<?=$labelTxtBroswer?>") $(this).val("");
+		}).blur(function() {
+			if ($.trim($(this).val())=="") $(this).val("<?=$labelTxtBroswer?>");
+		}).keyup(function() {
+			var txt=$.trim($(this).val());
+			var like = new RegExp(txt,"i");
+
+			$("#boxUserLstSeemytag li").each(function(i, li){
+				if(txt=="" || like.test($("strong",li).text()) || like.test($(":checkbox",li).val())) $(li).show();
+				else $(li).hide();
+			});
+		});
+
+		$("#hrefNoneBoxLstSeemytag").click(function(){
+			$("#contentLstUsersSeemytag :checkbox").attr("checked", false).change();
+		});
+		$("#hrefAllBoxLstSeemytag").click(function(){
+			$("#contentLstUsersSeemytag :checkbox").attr("checked", true).change();
+		});
+		$('input[type="checkbox"]').button().change(function(){
+			var li=$(this).parent().parent();// checkbox < p < li
+			if ($(this).is(':checked')){
+				li.css("background-color","FFFEE0");
+				li.find("label span").text("<?=$share?TAGS_OPTIONSHARENO:USERS_BROWSERFRIENDSLABELBTNUNINVITE?>");
+			}else{
+				li.css("background-color","FFF");
+				li.find("label span").text("<?=$share?TAGS_OPTIONSHARE:USERS_BROWSERFRIENDSLABELBTNINVITE?>");
+			}
+		}).change();
+	});
+</script>
