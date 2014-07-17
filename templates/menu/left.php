15ddff119782978273902e0654da754d3ade54f8
ramonJR <maxtri@hotmail.com>
2014-07-17 16:16:20 -0430

    no new
    Former-commit-id: e5c1b59001f001e2932b97b687bd46f27b59a1ed [formerly 67b571011a69d468f3c851bd09d8b5f1b380b0ff]
    Former-commit-id: 3136b68171207fd9062278d43259e0d40a3b7010

--- templates/menu/left.php
+++ templates/menu/left.php
@@ -284,7 +284,7 @@
 				interval=setInterval(function(){
 					if($(opc.layer).length>0){
 						//console.log('aqui')
-						getNews('refresh',opc);
+						// getNews('refresh',opc);
 					}else
 						clearEvents();
 				}, 30000);
@@ -294,7 +294,7 @@
 					$('#info-container').html($.smt.news);
 					delete $.smt.news;
 				}
-				getNews('reload',opc);
+				// getNews('reload',opc);
 				//fin-news
 
 				var menu=$('#menuLeft')[0];
