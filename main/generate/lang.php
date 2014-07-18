6c271d321da3f09e9e1aaf6f875cc6a410ab6587
wffranco <willemfranco@gmail.com>
2014-07-18 12:16:14 -0430

    arreglos traducciones
    Former-commit-id: d6dd4ecf54f3c13287a5493d8b2f45ad75eacc3a

--- main/generate/lang.php
+++ main/generate/lang.php
@@ -5,55 +5,47 @@ include('class/wconecta.class.php');
 
 $languageList=array('en','es');
 foreach($languageList as $code){
-	$list=CON::getObject('SELECT id_lenguage,label,text,text_help FROM translations_template');
+	$list=CON::getObject("SELECT DISTINCT tt.label, IFNULL(tr.text,tt.text) as `text` FROM translations_template tt LEFT JOIN translations tr ON tr.label=tt.label AND tr.cod='$code'");
 	$lang=array();
 	foreach($list as $el){
-		#verificamos si el lenguaje tiene traduccion
-		$lenguajes=CON::query("SELECT label,text,text_help FROM translations WHERE cod='$code' AND label LIKE '$el->label'");
-		#si no hay traduccion
-		if(CON::numRows($lenguajes)==0){ 
-			$lang[$el->label]=preg_replace('/[\r\n]+/',' ',$el->text);
-		}else{#si hay traduccion
-			$lenguaje=CON::fetchObject($lenguajes);//traducciones
-			$lang[$lenguaje->label]=preg_replace('/[\r\n]+/',' ',$el->text);
-		}
+		$lang[$el->label]=preg_replace('/[\r\n]+/',' ',utf8_encode($el->text));
 	}
+	$lang=array_merge(array('langcode'=>$code),$lang);
 	$lang['langcode']=$code;
 
-	#lenguajes en php
 	$json=json_encode($lang);
-	$salida=<<<PHPLAN
-<?php
-\$lang=json_decode('$json',true);
-PHPLAN;
-	// $array=str_replace('":"','"=>"',htmlentities(substr($json, 1, -1),ENT_NOQUOTES));
+	$json=preg_replace('/\\\\u([\d\w]{4})/','&#x$1;',$json);
+
+	#lenguajes en php
 	$array=str_replace('":"','"=>"',substr($json, 1, -1));
-	$array=preg_replace('/\\\\u([\d\w]{4})/','&#x$1;',$array);
 	$array=str_replace('<\\/','</',$array);
-	$salida=<<<PHPLAN
+	$salida=<<<PHPLANG
 <?php
 \$lang=array($array);
-function _(\$text='',\$format=){
+function lan(\$text='',\$format=false){
 	return (isset(\$lang[\$text])?\$lang[\$text]:\$text);
 }
-PHPLAN;
+PHPLANG;
 	file_put_contents("language/$code.php", $salida);
 
 	#lenguajes en js
 	$array=array();
+	$array['langcode']=$code;
 	foreach($lang as $key => $val){
 		if(substr($key,0,3)=='JS_'){
 			$array[$key]=utf8_encode($val);
 		}
 	}
 	$json=json_encode($array);//comentar para poner TODAS las traducciones en js
-	$salida=<<<JSLAN
+	$salida=<<<JSLANG
 var lang=$json;
 function lan(txt){ return (lang&&lang[txt]||txt||'');}
-JSLAN;
-	file_put_contents("js/language_$code.js", $salida);
+JSLANG;
+	file_put_contents("js/language_$code.js",$salida);
+
+	#funciones en js
 	ob_start();
-	require "js/funciones.js.php";
+	include("js/funciones.js.php");
 	$data=ob_get_contents();
 	ob_end_clean();
 	file_put_contents("js/funciones_$code.js",trim($data));
