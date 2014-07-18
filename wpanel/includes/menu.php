09644dea8369c7c94ec0cec402f5b2070a76bad7
Adrian esqueda <aesqueda14@gmail.com>
2014-07-18 15:55:30 -0430

    ajustes generales
    Former-commit-id: 6df8b943d7506f81bfd4bead5290d65f0bd3d791 [formerly 9a43fc6c09a3b975d27a778bc66dc709671283bf]
    Former-commit-id: 1b64359e9e790afcefbaa31398bf300ab742fec4

--- wpanel/includes/menu.php
+++ wpanel/includes/menu.php
@@ -6,112 +6,84 @@
         <td class="td_submenu" id="td-H1" onClick="inicio('../#creation&wpanel')">&raquo;&nbsp;Add Tags</td>
     </tr>
     <tr>
-
         <td class="td_submenu" id="td-H2" onClick="inicio('?url=vistas/viewTagWpanel.php')">&raquo;&nbsp;View Tags</td>
     </tr>
-
     <tr>
-
         <td width="95%">&nbsp;</td>
     </tr>
-
-
     <tr>
-
         <td class="td_menu"><strong style="font-size:12px">Users</strong></td>
     </tr>
     <tr>
-
         <td class="td_submenu" onClick="inicio('?url=vistas/users/usersRegistered.vista.php')">&nbsp;&raquo;&nbsp;Registered</td>
     </tr>
     <tr>
-
         <td class="td_submenu" onClick="inicio('?url=vistas/users/usersDevices.vista.php')">&nbsp;&raquo;&nbsp;Devices</td>
     </tr>
     <tr>
-
         <td class="td_submenu" onClick="inicio('?url=vistas/users/costs.vista.php')">&nbsp;&raquo;&nbsp;Subscription Plans</td>
     </tr>
     <tr>
-
         <td class="td_submenu" onClick="inicio('?url=vistas/users/manage.nonprofit.php')">&nbsp;&raquo;&nbsp;Nonprofit Account</td>
     </tr>
     <tr>
-
         <td width="95%">&nbsp;</td>
     </tr>
-
     <tr>
-
         <td class="td_menu"><strong style="font-size:12px">Payments</strong></td>
     </tr>
     <tr>
-
         <td class="td_submenu" onClick="inicio('?url=vistas/payments/paypal.php')">&nbsp;&raquo;&nbsp;PayPal</td>
     </tr>
     <tr>
-
         <td width="95%">&nbsp;</td>
     </tr>
-
     <tr>
-
         <td class="td_menu"><strong style="font-size:12px">Languages</strong></td>
     </tr>
     <tr>
-
         <td class="td_submenu" onClick="inicio('?url=vistas/languages/master.php')">&nbsp;&raquo;&nbsp;Master</td>
     </tr>
     <tr>
-
         <td class="td_submenu" onClick="inicio('?url=vistas/languages/sections.php')">&nbsp;&raquo;&nbsp;Sections Page</td>
     </tr>
     <tr>
-
         <td class="td_submenu" onClick="inicio('?url=vistas/languages/template.php')">&nbsp;&raquo;&nbsp;Template</td>
     </tr>
     <tr>
-
         <td class="td_submenu" onClick="inicio('?url=vistas/languages/translations.php')">&nbsp;&raquo;&nbsp;Translations</td>
     </tr>
+    <?php if ($_SESSION['wpanel_user']['tipo']=='1'){?>
     <tr>
-
+        <td class="td_submenu" onClick="inicio('../new.php?generate&language')">&nbsp;&raquo;&nbsp;Generate</td>
+    </tr>
+    <?php } ?>
+    <tr>
         <td width="95%">&nbsp;</td>
     </tr>
-    <!-- MENU EN ZEN CODING -->
+    <!-- MENU EN ZEN CODING --> 
     <!-- tr>td.td_menu>strong[style=font-size:12px]{TITULO MENU}
     tr*9>td.td_submenu[onClick=inicio('?url=vistas/dialogs.view.php?sc=$')]{&nbsp;&raquo;&nbsp;Contenido SubMenu}
     tr>td[width=95%]{$nbsp;} -->
     <tr>
-
         <td class="td_menu"><strong style="font-size:12px">Publicity</strong></td>
     </tr>
     <tr>
-
         <td class="td_submenu" onClick="inicio('?url=vistas/publicity/currency.php')">&nbsp;&raquo;&nbsp;Currency</td>
     </tr>
     <tr>
-
         <td class="td_submenu" onClick="inicio('?url=vistas/publicity/type_publicity.php')">&nbsp;&raquo;&nbsp;Types</td>
     </tr>
     <tr>
-
         <td class="td_submenu" onClick="inicio('?url=vistas/publicity/cost.php')">&nbsp;&raquo;&nbsp;Costs</td>
     </tr>
-
 	<tr>
-
         <td class="td_submenu" onClick="inicio('?url=vistas/publicity/manage.php')">&nbsp;&raquo;&nbsp;Manage</td>
     </tr>
-
-
 	<tr>
-
         <td class="td_submenu" onClick="inicio('?url=vistas/publicity/banners.php')">&nbsp;&raquo;&nbsp;Banners</td>
     </tr>
-
     <tr>
-
         <td width="95%">&nbsp;</td>
     </tr>
     <tr>
@@ -167,8 +139,6 @@
     <tr>
         <td width="95%">&nbsp;</td>
     </tr>
-
-
     <tr>
         <td class="td_menu"><strong style="font-size:12px">Config</strong></td>
     </tr>
