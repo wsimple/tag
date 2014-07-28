a4f408191687ab128598bf5c6dfb955fb5301bb5
wffranco <willemfranco@gmail.com>
2014-07-28 12:40:21 -0430

    arreglos configuracion y generador de seguridad
    Former-commit-id: b30358422533e75630020aafc6a2b7fa31eca8b2 [formerly 16554c68b9cbadf49448a8c3bc2319e88d5c4d12]
    Former-commit-id: 6faa0bac2d3f1340943167782550cd9c1c5541c6

--- main/generate/security.php
+++ main/generate/security.php
@@ -8,17 +8,17 @@ Se debe llamar al index de la siguiente manera:<br/><br/>
 tipos: local, main (servidor principal), sec (servidores secundarios)<br/>
 <?php
 }else{
-	$tipo=false;
 	$data=array();
+	if($_GET['tipo']!='') $data['tipo']=$_GET['tipo'];
+	elseif(isset($_GET['main'])) $data['tipo']='main';
+	elseif(isset($_GET['sec'])) $data['tipo']='sec';
+	elseif(isset($_GET['local'])) $data['tipo']='local';
 	if(!is_dir('.security')) mkdir('.security');
 	#tipos
-	if(isset($_GET['main'])){
-		$tipo='main';
-	}elseif(isset($_GET['sec'])){
-		$tipo='sec';
+	if($data['tipo']=='main'){
+	}elseif($data['tipo']=='sec'){
 		$data['path']='/';
 		$data['db']['host']='192.168.57.15';
-		// $data['db']['host']='68.109.244.200';//ip externo
 		$data['db']['user']='uservzla200';
 		$data['db']['pass']='-t@gvzlA_mysql';
 		$data['db']['data']='tagbum200';
@@ -26,23 +26,23 @@ tipos: local, main (servidor principal), sec (servidores secundarios)<br/>
 		$data['ftp']['user']='userimg';
 		$data['ftp']['pass']='-t@gvzlA_ftp';
 		$data['imgserver']='http://68.109.244.201/';
-	}elseif(isset($_GET['local'])){
-		$tipo='local';
+	}elseif($data['tipo']=='local'){
 		$data['path']='/tag/';
 		$data['db']['host']='localhost';
 		$data['db']['user']='root';
 		$data['db']['pass']='root';
 		$data['db']['data']='tagbum';
 	}
-	if($tipo){
-		$data['tipo']=$tipo;
+	if($data['tipo']){
+		$data['local']=($data['tipo']=='local');
+		$tipo=$data['tipo'];
 		if(isset($_GET['comp'])) //compress
 			$data='json_decode(base64_decode(base64_decode(\''.base64_encode(base64_encode(json_encode($data))).'\')))';
 		else //no compress
 			$data='json_decode(\''.json_encode($data).'\')';
 
-		$txt="<?php //$tipo\n\$config=$data;";
-		echo 'Instalando como tipo: '.$tipo.'<br/>';
+		$txt="<?php //tipo=$tipo\n\$config=$data;";
 		file_put_contents('.security/security.php',$txt);
+		echo "Instalando como tipo: $tipo<br/>";
 	}
 }
