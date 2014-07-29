3ab28bea7c0068b5c66524afd53570811e06d483
elibo <elijose.c@gmail.com>
2014-07-29 16:32:00 -0430

    Cambio de SDK FB a 2.0, Arreglo de compartir tag en facebook(No mostraba Imagen)
    Former-commit-id: 2c26291f358c64ba4e324733efa5a48eaf99c18a

--- views/tags/share/facebook.php
+++ views/tags/share/facebook.php
@@ -18,6 +18,6 @@
 	  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&appId=141402139297347&version=v2.0";
 	  fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));</script>
-	<div class="fb-share-button" data-href="http://tagbum.com<?=base_url('tag?tag='.$_GET['tag'].'&referee='.$_SESSION['ws-tags']['ws-user']['code'])?>"></div>
+	<div class="fb-share-button" data-href="<?=DOMINIO.base_url('tag?tag='.$_GET['tag'].'&referee='.$_SESSION['ws-tags']['ws-user']['code'])?>"></div>
 </body>
 </html>
