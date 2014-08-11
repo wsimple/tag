8fc2f8452bfdcf690c205c498b6a1b735671943a
wffranco <willemfranco@gmail.com>
2014-08-11 08:58:36 -0430

    video - instalacion y prueba de ffmpeg
    Former-commit-id: 87f5bb8bd3d0ccdf1ecc67100fe24461b288c0bc [formerly f9d5a03de27b8487f33dae5848967a85f975f9cb]
    Former-commit-id: 01c5dc3d6ab18b09b7f4bbe2e282370949c7ad4c

--- videos/videoserver.php
+++ videos/videoserver.php
@@ -14,6 +14,8 @@ http://ghamail.com/forum/read.php?6,139,139
 https://trac.ffmpeg.org/wiki/Using%20FFmpeg%20from%20PHP%20scripts
 composer: https://getcomposer.org/doc/00-intro.md
 
+lista x264 codecs: http://download.videolan.org/pub/videolan/x264/snapshots/
+
 
 test server 1: http://68.109.244.197/videos/videotest.php
 test server 2: http://68.109.244.198/videos/videotest.php
@@ -29,97 +31,114 @@ rpm -i http://www.nasm.us/pub/nasm/releasebuilds/2.09.10/linux/nasm-2.09.10-1.x8
 
 <hr/>
 mkdir ~/ffmpeg_sources
-cd  ~/ffmpeg_sources
 
-<b>H.264 video encoder.</b> 
+<b>H.264 video encoder.</b>
+modo 1 (ultima version):
+cd  ~/ffmpeg_sources
 git clone --depth 1 git://git.videolan.org/x264
 cd x264
-./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static --enable-shared
+./configure --enable-static --enable-shared --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
 make
 make install
 make distclean
-cd ..
 
-<b>AAC audio encoder.</b> 
+modo 2 (7-7-2014):
+cd  ~/ffmpeg_sources
+wget http://download.videolan.org/pub/videolan/x264/snapshots/x264-snapshot-20130701-2245-stable.tar.bz2
+tar jxvf x264-snapshot-20130701-2245-stable.tar.bz2
+cd x264-snapshot-20130701-2245-stable
+./configure --enable-static --enable-shared --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
+make
+make install
+make distclean
+
+
+<b>AAC audio encoder.</b>
+cd  ~/ffmpeg_sources
 git clone --depth 1 git://git.code.sf.net/p/opencore-amr/fdk-aac
 cd fdk-aac
 autoreconf -fiv
-./configure --prefix="$HOME/ffmpeg_build" --enable-shared
+./configure --enable-shared --prefix="$HOME/ffmpeg_build"
 make
 make install
 make distclean
-cd ..
 
 
 <b>MP3 audio encoder.</b>
+cd  ~/ffmpeg_sources
 curl -L -O http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
-tar xzvf lame-3.99.5.tar.gz
+tar zxvf lame-3.99.5.tar.gz
 cd lame-3.99.5
-./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-nasm --enable-shared
+./configure --enable-nasm --enable-shared --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
 make
 make install
 make distclean
-cd ..
 
-
-<b>Opus audio decoder and encoder.</b> 
-curl -O http://downloads.xiph.org/releases/opus/opus-1.1.tar.gz
-tar xzvf opus-1.1.tar.gz
-cd opus-1.1
-./configure --prefix="$HOME/ffmpeg_build" --enable-shared
-make
-make install
-make distclean
-cd ..
-
-
-<b>Ogg bitstream library.</b> 
+<b>Ogg bitstream library.</b>
+cd  ~/ffmpeg_sources
 curl -O http://downloads.xiph.org/releases/ogg/libogg-1.3.1.tar.gz
-tar xzvf libogg-1.3.1.tar.gz
+tar zxvf libogg-1.3.1.tar.gz
 cd libogg-1.3.1
-./configure --prefix="$HOME/ffmpeg_build" --enable-shared
+./configure --enable-shared --prefix="$HOME/ffmpeg_build"
 make
 make install
 make distclean
-cd ..
-
-
-<b>Vorbis audio encoder. Requires libogg.</b>
-curl -O http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.4.tar.gz
-tar xzvf libvorbis-1.3.4.tar.gz
-cd libvorbis-1.3.4
-./configure --prefix="$HOME/ffmpeg_build" --with-ogg="$HOME/ffmpeg_build" --enable-shared
-make
-make install
-make distclean
-cd ..
-
-
-<b>VP8/VP9 video encoder.</b>
-git clone --depth 1 https://chromium.googlesource.com/webm/libvpx.git
-cd libvpx
-./configure --prefix="$HOME/ffmpeg_build" --disable-examples --enable-shared
-make
-make install
-make clean
-cd ..
-
-<b>libavcodec55 (??)</b>
-rpm -Uhv ftp://rpmfind.net/linux/atrpms/el6-x86_64/atrpms/stable/libavcodec55-2.2.1-65.el6.x86_64.rpm
-
 
 <b>FFmpeg</b>
+cd  ~/ffmpeg_sources
 git clone --depth 1 git://source.ffmpeg.org/ffmpeg
 cd ffmpeg
 PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig"
 export PKG_CONFIG_PATH
-./configure --prefix="$HOME/ffmpeg_build" --extra-cflags="-I$HOME/ffmpeg_build/include" --extra-ldflags="-L$HOME/ffmpeg_build/lib" --bindir="$HOME/bin" --extra-libs=-ldl --enable-gpl --enable-nonfree --enable-libfdk_aac --enable-libmp3lame --enable-libopus --enable-libvorbis --enable-libvpx --enable-libx264 --enable-shared
+./configure --prefix="$HOME/ffmpeg_build" --extra-cflags="-I$HOME/ffmpeg_build/include" --extra-ldflags="-L$HOME/ffmpeg_build/lib" --bindir="$HOME/bin" --extra-libs=-ldl --enable-gpl --enable-nonfree --enable-libfdk_aac --enable-libmp3lame --enable-libx264 --enable-shared
 make
 make install
 make distclean
 hash -r
 . ~/.bash_profile
 cd ..
+
+<b>acceso a librerias:</b>
+nano /etc/ld.so.conf
+agregar:
+<span class="block">/usr/local/libevent-1.4.14b/lib
+/usr/local/lib</span>
+
+
+<hr/>
+CON MAS CODECS:
+
+<b>Opus audio decoder and encoder.</b>
+cd  ~/ffmpeg_sources
+curl -O http://downloads.xiph.org/releases/opus/opus-1.1.tar.gz
+tar zxvf opus-1.1.tar.gz
+cd opus-1.1
+./configure --enable-shared --prefix="$HOME/ffmpeg_build"
+make
+make install
+make distclean
+
+<b>Vorbis audio encoder. Requires libogg.</b>
+cd  ~/ffmpeg_sources
+curl -O http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.4.tar.gz
+tar zxvf libvorbis-1.3.4.tar.gz
+cd libvorbis-1.3.4
+./configure --enable-shared --prefix="$HOME/ffmpeg_build" --with-ogg="$HOME/ffmpeg_build"
+make
+make install
+make distclean
+
+<b>VP8/VP9 video encoder.</b>
+cd  ~/ffmpeg_sources
+git clone --depth 1 https://chromium.googlesource.com/webm/libvpx.git
+cd libvpx
+./configure --disable-examples --enable-shared --prefix="$HOME/ffmpeg_build"
+make
+make install
+make clean
+
+<b>FFmpeg</b>
+./configure --prefix="$HOME/ffmpeg_build" --extra-cflags="-I$HOME/ffmpeg_build/include" --extra-ldflags="-L$HOME/ffmpeg_build/lib" --bindir="$HOME/bin" --extra-libs=-ldl --enable-gpl --enable-nonfree --enable-libfdk_aac --enable-libmp3lame --enable-libopus --enable-libvorbis --enable-libvpx --enable-libx264 --enable-shared
 
 <hr/>
 <b>FFmpeg-devel</b>
@@ -157,6 +176,7 @@ hash -r
 <b>Remover algunos prerequisitos</b>
 rpm -e yasm
 rpm -e nasm
+
 
 <?php
 $text=ob_get_contents();
