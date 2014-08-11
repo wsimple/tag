dc621ff3c2903d5b6a3b0a99df9c542ad10c6064
wffranco <willemfranco@gmail.com>
2014-08-11 08:56:57 -0430

    Acceso a imageresizer como modulo desde el index
    * ajuste imagen.php para permitir llamado normal y por seccion
    * permitido desde index el llamado a traves de la seccion 'image'
    
    
    Former-commit-id: 023ca390ea4d9f8884ba8734214e0a2a0a6e7083 [formerly c884449fa814c0de512bc9a2aaf0ebb856f239ad]
    Former-commit-id: 6861c984c6ad80e0ff4e860429d789420fb52a76

--- includes/imagen.php
+++ includes/imagen.php
@@ -1,29 +1,40 @@
 <?php
-	include_once('resizeimage.inc.php');
-    include_once('functions.php');
-	if(isset($_GET['debug'])) die($_GET[img]);
-	header('Content-Type: image/jpeg');
-	$rimg=new RESIZEIMAGE(relativePath($_GET[img]));
-	
-	switch ($_GET['tipo']){
-	        case 1:	$rimg->resize_percentage($_GET['porc']);       break;
-	        case 2: $rimg->resize($_GET['ancho'], $_GET['alto']); break;
-			case 3: $rimg->resize_limitwh($_GET['ancho'], 0);
-	}
-			
-	$rimg->close();
-	/*
-	<!-- 
-			+ --------------------------------------------------------- +
-			|                                                           |
-			| 	Desarrollado por: Gustavo A. Ocanto C.                  |
-			| 	Email: gustavoocanto@gmail.com / info@websarrollo.com   |
-			| 	Tel�fono: 0414-428.42.30 / 0245-511.38.40               |
-			| 	Web: http://www.gustavoocanto.com                       |
-			|        http://www.websarrollo.com                         |
-			| 	Valencia, Edo. Carabobo - Venezuela                     |
-			|                                                           |
-			+ --------------------------------------------------------- +
-	-->
-	*/	
-?>
+include_once('config.php');
+include_once('resizeimage.inc.php');
+function relative_path($url){
+	global $config;
+	return str_replace($config->dominio,$config->relpath,$url);
+}
+$tipo=$_GET['tipo'];
+$porc=$_GET['porc'];
+if(!$_GET['tipo']){$tipo=1;$porc=100;}
+$img='';
+if($section=='image'&&count($params)>0) $img=$config->imgserver.'/img/'.implode('/',$params);
+if(isset($_GET['img'])) $img=$_GET['img'];
+$img=relative_path($img);
+
+$rimg=new RESIZEIMAGE($img);
+switch($tipo){
+	case 1:	$rimg->resize_percentage($porc);break;
+	case 2: $rimg->resize($_GET['ancho'], $_GET['alto']);break;
+	case 3: $rimg->resize_limitwh($_GET['ancho'], 0);
+}
+header('Content-Type: image/jpeg');
+$rimg->close();
+die();
+
+/*
+<!--
+	+-----------------------------------------------------------+
+	|															|
+	| 	Desarrollado por: Gustavo A. Ocanto C.					|
+	| 	Modificado por: Willem F. Franco C.						|
+	| 	Email: gustavoocanto@gmail.com / info@websarrollo.com	|
+	| 	Telefono: 0414-428.42.30 / 0245-511.38.40				|
+	| 	Web: http://www.gustavoocanto.com						|
+	|        http://www.websarrollo.com							|
+	| 	Valencia, Edo. Carabobo - Venezuela						|
+	|															|
+	+-----------------------------------------------------------+
+-->
+*/
