783c9c898a1ee45e24924f104f034807bc80b2c5
wffranco <willemfranco@gmail.com>
2014-08-14 17:15:15 -0430

    ajustes imagenes
    Former-commit-id: 6e402a8d94de2c2aec61446a8d3ebc497fa21417 [formerly a176d0d40cefc5a263b0f159ed5b01db14851bff]
    Former-commit-id: f40ba50b0679e87180e346b21e4ae69a789503d3

--- img.php
+++ img.php
@@ -1,5 +1,6 @@
 <?php
 //die($_SERVER['SCRIPT_NAME'].', '.$_SERVER['REQUEST_URI']);
+if(!preg_match('/\.[a-z0-9]{3,}$/',$_SERVER['REQUEST_URI'])) die();
 $img='img/not-found.jpg';
 if(file_exists('img/'.$_SERVER['REQUEST_URI'])&&is_file('img/'.$_SERVER['REQUEST_URI']))
 	$img='img/'.$_SERVER['REQUEST_URI'];
