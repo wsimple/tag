b1964f622169443248b2014e3100bab0fd6423b2
wffranco <willemfranco@gmail.com>
2014-08-14 17:23:21 -0430

    correccion de imagenes
    * validacion de get en la url
    
    
    Former-commit-id: a75e1239c801676afb03f272469a638408803aed

--- img.php
+++ img.php
@@ -1,8 +1,9 @@
 <?php
 //die($_SERVER['SCRIPT_NAME'].', '.$_SERVER['REQUEST_URI']);
-if(!preg_match('/\.[a-z0-9]{3,}$/',$_SERVER['REQUEST_URI'])) die();
+$url=array_shift(split('?',$_SERVER['REQUEST_URI']));
+if(!preg_match('/\.[a-z0-9]{3,}$/',$url)) die();
 $img='img/not-found.jpg';
-if(file_exists('img/'.$_SERVER['REQUEST_URI'])&&is_file('img/'.$_SERVER['REQUEST_URI']))
-	$img='img/'.$_SERVER['REQUEST_URI'];
+if(file_exists('img/'.$url)&&is_file('img/'.$url))
+	$img='img/'.$url;
 header('Content-Type: '.image_type_to_mime_type(exif_imagetype($img)));
 include($img);
--- img/index.php
+++ img/index.php
@@ -1,6 +1,7 @@
 <?php
 //die($_SERVER['SCRIPT_NAME'].', '.$_SERVER['REQUEST_URI']);
-if(!preg_match('/\.[a-z0-9]{3,}$/',$_SERVER['REQUEST_URI'])) die();
+$url=array_shift(split('?',$_SERVER['REQUEST_URI']));
+if(!preg_match('/\.[a-z0-9]{3,}$/',$url)) die();
 $img='not-found.jpg';
 header('Content-Type: '.image_type_to_mime_type(exif_imagetype($img)));
 include($img);
