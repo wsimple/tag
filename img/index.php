e27654ccb39ed90fb0c2a3b023fb1494dc580844
wffranco <willemfranco@gmail.com>
2014-08-14 17:18:07 -0430

    corregido error en ruta
    Former-commit-id: e8448b2d874d0e65f59853985f14409879d21763

--- img/index.php
+++ img/index.php
@@ -1,6 +1,6 @@
 <?php
 //die($_SERVER['SCRIPT_NAME'].', '.$_SERVER['REQUEST_URI']);
 if(!preg_match('/\.[a-z0-9]{3,}$/',$_SERVER['REQUEST_URI'])) die();
-$img='img/not-found.jpg';
+$img='not-found.jpg';
 header('Content-Type: '.image_type_to_mime_type(exif_imagetype($img)));
 include($img);
