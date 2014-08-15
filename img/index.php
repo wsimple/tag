18d076257a41ab8b658d43af603c5cdd74934de4
wffranco <willemfranco@gmail.com>
2014-08-15 10:38:15 -0430

    ajuste en validacion de mime type
    Former-commit-id: 2d8ce18714fa3876aa8d1489dc2ea2f1ca142d8b

--- img/index.php
+++ img/index.php
@@ -3,5 +3,7 @@ $url=array_shift(explode('?',$_SERVER['REQUEST_URI']));
 // if(!preg_match('/\.[a-z0-9]{3,}$/',$url)) die($_SERVER['SCRIPT_NAME'].', '.$_SERVER['REQUEST_URI'].', '.$url);
 if(!preg_match('/\.[a-z0-9]{3,}$/',$url)) die();
 $img='not-found.jpg';
-header('Content-Type: '.image_type_to_mime_type(exif_imagetype($img)));
+$finfo=finfo_open(FILEINFO_MIME_TYPE);
+header('Content-Type: '.finfo_file($finfo,$img));
+// header('Content-Type: '.image_type_to_mime_type(exif_imagetype($img)));
 include($img);
