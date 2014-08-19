b7f2462080adc240bcb8013f9a9617690be0c468
wffranco <willemfranco@gmail.com>
2014-08-19 17:54:13 -0430

    ajuste de cookies para subdominios
    Former-commit-id: 6538e5e3b108f4247acf7a203e903ecd298cbf75

--- includes/session.php
+++ includes/session.php
@@ -1,27 +1,22 @@
 <?php
 session_start();
-if($_SESSION['ws-tags']['ws-user']['id']!=''&&$_COOKIE['__logged__']==''){
-	setcookie('__logged__',md5($_SESSION['ws-tags']['ws-user']['time']),time()+60*60*24*30,'/');
-	if($_SERVER['SERVER_NAME'])setcookie('__logged__',md5($_SESSION['ws-tags']['ws-user']['time']),time()+60*60*24*30,'/');
+$__val=$_SESSION['ws-tags']['ws-user']['id']!=''?$_SESSION['ws-tags']['ws-user']['id']:NULL;
+$__time=time()+($__val?60*30:-3600);
+if( $_SESSION['ws-tags']['ws-user']['id']=='' xor $_COOKIE['__logged__']=='' ){
+	setcookie('__logged__',$__val?md5($_SESSION['ws-tags']['ws-user']['time']):NULL,$__time,'/');
 }
-if($_SESSION['ws-tags']['ws-user']['id']==''&&$_COOKIE['__logged__']!=''){
-	setcookie('__logged__',NULL,time()-3600,'/');
-}
-#guardar id en las cookies para uso en subdominios. Solo en el dominio especificado.
+#guardar cookies para subdominios. Solo en el dominio principal.
 $__dominio='tagbum.com';
 if(isset($_SESSION['ws-tags'])&&strpos($_SERVER['SERVER_NAME'],$__dominio)!==FALSE){
-	#regex dominio: /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9]\.[a-zA-Z]{2,}$/
 	$__dominio='.'.$__dominio;
-	if($_SESSION['ws-tags']['ws-user']['id']!=''&&$_COOKIE['__uid__']=='')
-		setcookie('__uid__',md5($_SESSION['ws-tags']['ws-user']['id']),time()+60*30,'/',$__dominio);
-	if($_SESSION['ws-tags']['ws-user']['id']==''&&$_COOKIE['__uid__']!='')
-		setcookie('__uid__',NULL,time()-3600,'/',$__dominio);
+	setcookie('__uid__',$__val?md5($__val):NULL,$__time,'/',$__dominio);
 }
-unset($__dominio);
+unset($__dominio,$__val,$__time);
 
 if(	!strpos($_SERVER['PHP_SELF'],'carouselTags.view.php')	&&	!strpos($_SERVER['PHP_SELF'],'registerTabs.view.php') &&
 	!strpos($_SERVER['PHP_SELF'],'resendPassword.view.php')	&&	!$_SESSION['ws-tags']['ws-user']['id'] &&
 	$_POST['asyn']=='1' ){
 		die('1');
 }
+#regex dominios: /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9]\.[a-zA-Z]{2,}$/
 ?>
