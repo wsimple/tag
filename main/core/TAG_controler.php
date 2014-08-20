d76e2d0b59a2750378bca310008580614d484e4d
wffranco <willemfranco@gmail.com>
2014-08-20 14:05:58 -0430

    ajuste mvc
    * agregados ejemplos de uso
    * desactivada la visualizacion del control ejemplo si esta en modo debug
    
    
    Former-commit-id: 636ace870d8f9e5a9e7c01d6f8545e0f617ea2a0 [formerly 91c25bbc8f6b48b96f75fc7257a7ba95557c0a86]
    Former-commit-id: a9b7706309e7c75168c9b83af61ae97cd1915b5d

--- main/core/TAG_controler.php
+++ main/core/TAG_controler.php
@@ -1,7 +1,19 @@
 <?php
 class TAG_controler extends TAG_functions{
-	function __construct(){
-		parent::__construct();
+	private $no_method;
+	function __construct($params=array()){
+		parent::__construct($params);
+		$name=strtolower(get_class($this));
+		if(is_file("main/models/$name.php")) $this->load_model($name);
 	}
 	function __destruct(){}
+	public function disable_methods(){
+		$this->no_method=true;
+	} 
+	public function enable_methods(){
+		$this->no_method=false;
+	}
+	public function use_methods(){
+		return !$this->no_method;
+	}
 }
