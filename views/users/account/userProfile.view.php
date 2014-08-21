3d10f222758e8cc16d8e8345048f252b5d392bd8
ramonJR <maxtri@hotmail.com>
2014-08-21 10:50:01 -0430

    User photo round on tag and userprofile
    Former-commit-id: af8cc4589bd1bf0c75865ff061312240d6672f54

--- views/users/account/userProfile.view.php
+++ views/users/account/userProfile.view.php
@@ -4,35 +4,31 @@ include ("../../../includes/functions.php");
 include ("../../../includes/config.php");
 include ("../../../class/wconecta.class.php");
 include ("../../../includes/languages.config.php");
-$sql = "
-		SELECT
-			concat(name, ' ', last_name) AS nameUsr,
-			screen_name,
-			date_birth,
-			followers_count,
-			following_count,
-			tags_count,
-			profile_image_url,
-			id,
-			pay_bussines_card,
-			md5(CONCAT(id, '_', email, '_', id)) AS code_friend,
-			show_my_birthday,
-			username,
-			country
-		FROM users
-		WHERE md5(id)='".$_GET['id']."'
-	";
-$idUserprodile = campo('users', 'md5(id)', $_GET['id'], 'id');
-$friend        = $GLOBALS['cn']->queryRow($sql);
+
+$friend = CON::getRow("SELECT 
+						concat(u.name, ' ', u.last_name) AS nameUsr,
+						u.screen_name,
+						u.date_birth,
+						u.followers_count,
+						u.following_count,
+						u.tags_count,
+						u.profile_image_url,
+						(SELECT count(t.id) FROM tags t WHERE t.id_user=u.id AND t.status = 1) AS nTags,
+						(SELECT count(t.id) FROM tags t WHERE t.id_creator=u.id AND t.status = 1) AS nPTags,
+						(SELECT count(b.id) FROM business_card b WHERE b.id_user = u.id AND b.type=0 ) AS nBCar,
+						(SELECT ul.id_user FROM users_links ul WHERE ul.id_friend=u.id AND ul.id_user=? LIMIT 1) AS follower,
+						u.id,
+						u.pay_bussines_card,
+						md5(CONCAT(u.id, '_',u.email, '_',u.id)) AS code_friend,
+						u.show_my_birthday,
+						u.username,
+						u.country FROM users u
+						WHERE md5(u.id)=?",array($_SESSION['ws-tags']['ws-user']['id'],$_GET['id']));
+
 ?>
-<script type="text/javascript">
-	$(function(){
-//		$('#userProfileDialog [onclick],#userProfileDialog [action],#userProfileDialog a').button();
-	});
-</script>
 <div data-role="page" id="userProfileDialog">
 	<div data-role="content" id="userProfile" class="userProfileStyle">
-		<img src="<?=FILESERVER.getUserPicture($friend['code_friend'].'/'.$friend['profile_image_url'], 'img/users/default.png')?>" width="60" height="60" style="float:right" />
+		<img src="<?=FILESERVER.getUserPicture($friend['code_friend'].'/'.$friend['profile_image_url'], 'img/users/default.png')?>" width="60" height="60" style="float:right;border-radius:50%;" />
 		<ul class="list_inline">
 			<li style="margin-bottom:10px;">
 				<strong><?=USER_LBLNAME?>:</strong> <?=$friend['nameUsr']?>
@@ -41,73 +37,46 @@ $friend        = $GLOBALS['cn']->queryRow($sql);
 				<strong><?=SIGNUP_LBLSCREENNAME?>: </strong> <?=$friend['screen_name']?>
 			</li>
 			<li style="margin-bottom:10px;">
-				<strong><?=USER_LBLBIRTHDATE?>: </strong> <?=maskBirthday($friend['date_birth'], (($_SESSION['ws-tags']['ws-user']['show_birthday'] != '')?$_SESSION['ws-tags']['ws-user']['show_birthday']:$friend['show_my_birthday']))?>
+				<strong><?=USER_LBLBIRTHDATE?>: </strong> <?=maskBirthday($friend['date_birth'],$friend['show_my_birthday'])?>
 			</li>
 			<li style="margin-bottom:10px;">
 				<strong><?=USER_LBLFOLLOWERS?>: </strong>
-				<input type="button" id="followers" onclick="<?php if ($friend['followers_count'] > 0) {?>friendsUser('<?=$friend[screen_name].' : '.USER_LBLFOLLOWERS?>','?uid=<?=md5($friend[id])?>&follower=1&fid=<?=md5($idUserprodile)?>'); <?php }?>"
+				<input type="button" id="followers" onclick="<?php if ($friend['followers_count'] > 0) {?>friendsUser('<?=$friend[screen_name].' : '.USER_LBLFOLLOWERS?>','?uid=<?=md5($friend[id])?>&follower=1&fid=<?=md5($friend['id'])?>'); <?php }?>"
 					value="<?=mskPoints($friend['followers_count'])?>"/>
 
 				<strong><?=USER_LBLFRIENDS?>: </strong>
-				<input type="button" id="friends" onclick="<?php if ($friend['following_count'] > 0) {?>friendsUser('<?=$friend[screen_name].' : '.USER_LBLFRIENDS?>','?uid=<?=md5($friend[id])?>&fid=<?=md5($idUserprodile)?>'); <?php }?>"
+				<input type="button" id="friends" onclick="<?php if ($friend['following_count'] > 0) {?>friendsUser('<?=$friend[screen_name].' : '.USER_LBLFRIENDS?>','?uid=<?=md5($friend[id])?>&fid=<?=md5($friend['id'])?>'); <?php }?>"
 					value="<?=mskPoints($friend['following_count'])?>"/>
-<?php
-$nTags = current($GLOBALS['cn']->queryRow('
-						SELECT count(id) AS nTags
-						FROM tags
-						WHERE md5(id_user)="'.$_GET['id'].'" AND status = 1
-					'));
-?>
 				<strong><?=MAINMNU_HOME?>: </strong>
-				<input type="button" id="tags" action="tagsUser,<?=$nTags?>,<?=$friend['screen_name']?>:Tags,<?=md5($friend['id'])?>"
-					value="<?=$nTags//mskPoints($friend[tags_count])?>"/>
+				<input type="button" id="tags" action="tagsUser,<?=$friend['nTags']?>,<?=$friend['screen_name']?>:Tags,<?=md5($friend['id'])?>"
+					value="<?=$friend['nTags']?>"/>
 			</li>
 			<li style="padding-top:10px;margin-bottom:10px;text-align:center;">
-<?php
-$tagStatus = $GLOBALS['cn']->query("SELECT status FROM tags WHERE id_creator='".$friend['id']."' AND  status = '1'");
-if (mysql_num_rows($tagStatus) > 0) {
-	?>
-						<input type="button" id="Mytag" action="personalTags,<?=$friend['screen_name'].' : '.USERPROFILE_TITLEMYUNIQUETAG?>,<?=md5($friend['id'])?>"
-							value="<?=USERPROFILE_LABLEBTNVIEWMYTAGS?>"/>
-	<?php
-}
-//IF HAS PAID AND DEFINED PERSONAL BUSINESS CARD -> SHOW BUTTON ON PROFILE
-$haveBusinessCard = $GLOBALS['cn']->query("SELECT type FROM business_card WHERE id_user = '".$friend['id']."' AND type=0");
-if (mysql_num_rows($haveBusinessCard) > 0 && !isset($_GET['calledProfile'])) {?>
-						<input type="button" id="bussi" onclick="message('messages','<?=USERPROFILE_TITLEBUSINESSCARD?>','','',430,300,DOMINIO+'views/users/account/business_card/businessCard_dialog.view.php?uid=<?=md5($friend['id'])?>');"
-							value="<?=USERPROFILE_LINKSHOWBUSINESSCARD?>"/>
-	<?php
-}?>
-</li>
+				<?php if ($friend['nPTags']> 0){ ?>
+					<input type="button" id="Mytag" action="personalTags,<?=$friend['screen_name'].' : '.USERPROFILE_TITLEMYUNIQUETAG?>,<?=md5($friend['id'])?>" value="<?=USERPROFILE_LABLEBTNVIEWMYTAGS?>"/>
+				<?php }
+				//IF HAS PAID AND DEFINED PERSONAL BUSINESS CARD -> SHOW BUTTON ON PROFILE
+				if ($friend['nBCar']> 0 && !isset($_GET['calledProfile'])) {?>
+					<input type="button" id="bussi" onclick="message('messages','<?=USERPROFILE_TITLEBUSINESSCARD?>','','',430,300,DOMINIO+'views/users/account/business_card/businessCard_dialog.view.php?uid=<?=md5($friend['id'])?>');" value="<?=USERPROFILE_LINKSHOWBUSINESSCARD?>"/>
+				<?php } ?>
+			</li>
 		</ul>
 		<br/>
-<?php
-if ($_SESSION['ws-tags']['ws-user']['id'] && $_SESSION['ws-tags']['ws-user']['id'] != $friend['id']) {
-	$sql = "
-				SELECT id_user
-				FROM users_links
-				WHERE id_friend='".$friend['id']."' AND id_user='".$_SESSION['ws-tags']['ws-user']['id']."'
-				LIMIT 1
-			";
-	$follower = (mysql_num_rows($GLOBALS['cn']->query($sql)) == 1);
-	?>
-					<div style="width:100%; text-align:center;">
-						<input type="button" id="dialog_btn_link_<?=md5($friend['id'])?>" <?=$follower?'style="display:none; margin-right: 20px;color: white;"':'style="color: white;"'?> action="linkUser,#div_<?=md5($friend['id'])?>,<?=md5($friend['id'])?>,,,true"
-							value="<?=USER_BTNLINK?>"/>
-						<input type="button" id="dialog_btn_unlink_<?=md5($friend['id'])?>" <?=$follower?'style="color: white;"':'style="display:none; margin-right: 20px;color: white;"'?> action="linkUser,#div_<?=md5($friend['id'])?>,<?=md5($friend['id'])?>,animate,,true"
-							value="<?=USER_BTNUNLINK?>"/>
+<?php if ($_SESSION['ws-tags']['ws-user']['id'] != $friend['id']) { ?>
+		<div style="width:100%; text-align:center;">
+			<input type="button" id="dialog_btn_link_<?=md5($friend['id'])?>" <?=$friend['follower']?'style="display:none; margin-right: 20px;color: white;"':'style="color: white;"'?> action="linkUser,#div_<?=md5($friend['id'])?>,<?=md5($friend['id'])?>,,,true"
+				value="<?=USER_BTNLINK?>"/>
+			<input type="button" id="dialog_btn_unlink_<?=md5($friend['id'])?>" <?=$friend['follower']?'style="color: white;"':'style="display:none; margin-right: 20px;color: white;"'?> action="linkUser,#div_<?=md5($friend['id'])?>,<?=md5($friend['id'])?>,animate,,true"
+				value="<?=USER_BTNUNLINK?>"/>
 	<?php
-	if ($friend['username'] != '') {
-		$externalProfileId = 'eprofile?usr='.$friend['username'];
-	} else {
-		$externalProfileId = "profile?sc=6&userIdExternalProfile=".md5($friend['id']);
-	}
+	if ($friend['username'] != '') $externalProfileId = $friend['username'];
+	else $externalProfileId = "user/".md5($friend['id']);
 	?>
-						<input type="button" id="externalProfile" action="exProfile,<?=$externalProfileId?>" style="margin: 0px 25px; color: white;"
-							value="<?=EXTERNALPROFILE?>"/>
-					</div>
-					<div id="error"></div>
-	<?php }?>
+			<input type="button" id="externalProfile" action="exProfile,<?=$externalProfileId?>" style="margin: 0px 25px; color: white;"
+				value="<?=EXTERNALPROFILE?>"/>
+		</div>
+		<div id="error"></div>
+	<?php } ?>
 </div><!-- /content -->
 </div><!-- /page -->
 <script type="text/javascript">
