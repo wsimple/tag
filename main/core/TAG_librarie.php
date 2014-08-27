3af436c2d20509bcebd27406bb5a3b55b94acf6f
wffranco <willemfranco@gmail.com>
2014-08-27 17:30:34 -0430

    ajustes mvc
    Former-commit-id: 5c5ed667affe5a712cd93b77f91aad01018a977e

--- main/core/TAG_librarie.php
+++ main/core/TAG_librarie.php
@@ -1,4 +1,18 @@
 <?php
-class TAG_librarie extends BASE_librarie{
+class TAG_librarie{
 	#configuracion general para todas las librerias
+	var $control,$db;
+	function __construct($parent=false){
+		if(!$parent){ global $control; $parent=$control; }
+		$this->control=$parent;
+		$this->db=$parent->db;
+	}
+	function __destruct(){
+	}
+	function load_lib($name){
+		$this->control->load_lib($name);
+	}
+	function load_libs($names){
+		$this->control->load_libs($names);
+	}
 }
---
+++ main/views/partial/footer.php
@@ -0,0 +1,9 @@
+<?php if($is_logged){ ?>
+
+<?php }else{ ?>
+
+<?php }
+$control->flush_echo();
+?>
+</body>
+</html>
