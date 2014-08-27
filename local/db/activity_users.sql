e633ecf2c4888a11471ca0a7ac44dcf054c6a264
wffranco <willemfranco@gmail.com>
2014-08-27 18:11:29 -0430

    limpieza activity_user
    Former-commit-id: 8e72b89211cb119f30ef451c8f0267390042eda1

--- local/db/activity_users.sql
+++ local/db/activity_users.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50614
 File Encoding         : 65001
 
-Date: 2014-08-27 17:19:03
+Date: 2014-08-27 18:10:54
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -28,10 +28,8 @@ CREATE TABLE `activity_users` (
   `HTTP_USER_AGENT` varchar(200) CHARACTER SET utf8 NOT NULL,
   `session_id` varchar(50) CHARACTER SET utf8 NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of activity_users
 -- ----------------------------
-INSERT INTO `activity_users` VALUES ('1', '124', 'f5731f19900e907d55498802fa0be5f2', '2014-08-27 17:18:54', '::1', 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36', 'smdi3tjugurjrs5i6dgs4pt8g4');
-INSERT INTO `activity_users` VALUES ('2', '124', 'f5731f19900e907d55498802fa0be5f2', '2014-08-27 17:19:01', '192.168.1.129', 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36', 'n1atfhbgitlheasnps2m3vpnt4');
