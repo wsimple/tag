258e6b7e215045c737aeb6d874e462626e8ae04c
wffranco <willemfranco@gmail.com>
2014-08-28 14:38:54 -0430

    libreria de lenguaje
    Former-commit-id: 6b3a498f1ecbcb88070614ac6a146e60fbd3868e [formerly 930e86ce893fa1d9e16273fb7510268dfd58e494]
    Former-commit-id: 3e197181b4ec7a063d290145ea297221dbc44fbc

---
+++ main/libraries/lang.php
@@ -0,0 +1,48 @@
+<?php
+# Libreria para manejo de lenguaje
+class Lang_lib extends TAG_librarie{
+	private $lang,$_code;
+	function __construct($control=false){
+		parent::__construct($control);
+		#lenguajes permitidos
+		$languages=array('en','es');
+		#deteccion de lenguaje
+		$actual='';
+		if($_GET['lang']!=''){
+			$actual=$_GET['lang'];
+		}else{
+			#si llego el lenguaje y el usuario esta logueado
+			if ($_SESSION['ws-tags']['ws-user']['language']!=''){
+				$_SESSION['ws-tags']['language']=$_SESSION['ws-tags']['ws-user']['language'];
+			}elseif($_POST['lang']!=''){
+				$_SESSION['ws-tags']['language']=$_POST['lang'];
+				@header('Location:'.$_POST['actualUrl']);//Url Actual donde se cambiara el idioma
+			}
+			#detecta el idioma segun la ip del usuario si no esta logeado
+			if($_GET['lang']==''&&empty($_SESSION['ws-tags']['language'])){
+				if(preg_match('/^(local\.|localhost|127\.|192\.168\.)/',$_SERVER['SERVER_NAME'])){
+					$_SESSION['ws-tags']['language']=substr($_SERVER['HTTP_ACCEPT_LANGUAGE'],0,2);
+				}else{
+					$ip_num=sprintf("%u",ip2long($_SERVER['REMOTE_ADDR']));
+					$locale=$this->db->getVal('SELECT idioma FROM geo_ip WHERE ? BETWEEN start AND end',array($ip_num));
+					$_SESSION['ws-tags']['language']=$locale;
+				}
+			}
+			$actual=$_SESSION['ws-tags']['language'];
+		}
+		#si el lenguaje no esta permitido, cargamos el ingles
+		if(!in_array($actual,$languages)) $actual='en';
+		$this->_code=$actual;
+		#carga de traducciones
+		$lang=array();
+		include("language/$actual.php");
+		$this->lang=$lang;
+	}
+	function code(){
+		return $this->_code;
+	}
+	function get($text='',$format=false){
+		$lang=&$this->lang;
+		return (isset($lang[$text])?$lang[$text]:$text);
+	}
+}
