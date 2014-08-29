d7dd0e61db63ff2115d16746bc84632b34f04b51
wffranco <willemfranco@gmail.com>
2014-08-29 17:14:12 -0430

    plugin fileupload, vista para upload de video.
    Former-commit-id: 444f240e2bdfcb494e087a7fed8bffaca468d74e [formerly 7cf743e8ad5c7976200baef778f78ae5430bb768]
    Former-commit-id: 6061b36ae8f7906c96b94123ec69d691442b00bc

--- main/controllers/video.php
+++ main/controllers/video.php
@@ -1,7 +1,21 @@
 <?php
 
 class Video extends TAG_controller{
-	function upload(){
-		$this->view('video');
+	function __onload(){
+		// echo 'dir='.dirname($_SERVER['SCRIPT_NAME']).'<br>';
+	}
+	function index(){
+		echo 'Principal. Empty.<br>';
+		echo '<a href="video/dialog/upload">Upload</a>';
+	}
+	function dialog($type=''){
+		switch($type){
+			case 'upload':
+				$this->view('dialogs/video_upload');
+			break;
+			default:
+				echo 'Dialog not exist.';
+
+		}
 	}
 }
