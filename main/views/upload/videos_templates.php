3e574c1a01d72eaed8035e95719cc9e5cae3a2e9
wffranco <willemfranco@gmail.com>
2014-09-04 16:52:34 -0430

    arreglos dialogo
    Former-commit-id: 0b13525ec3b18e525d2797880445f3d93515c479

--- main/views/upload/videos_templates.php
+++ main/views/upload/videos_templates.php
@@ -2,7 +2,10 @@
 <link rel="stylesheet" href="css/fileupload/jquery.fileupload.css">
 <link rel="stylesheet" href="css/fileupload/jquery.fileupload-ui.css">
 <style>
-	#tag-multime{
+	.upload-panel{
+		width:750px;
+	}
+	.upload-menu{
 		min-height: 50px;
 		width: 100%;
 		margin-bottom: 15px;
@@ -10,7 +13,7 @@
 		background-color: rgba(233,233,233,0.2);
 		-webkit-box-shadow: inset 1px 1px 0 rgba(233,233,233,0.1),inset 0 -1px 0 rgba(233,233,233,0.07);
 	}
-	#tag-multime div{
+	.upload-menu div{
 		background: transparent;
 		border: none;
 		color: #222;
@@ -19,8 +22,8 @@
 		margin: 0 18px;
 		padding: 16px 4px 7px;
 	}
-	#tag-multime div:hover,
-	#tag-multime div.active{
+	.upload-menu div:hover,
+	.upload-menu div.active{
 		background: transparent !important;
 		border-bottom: 2px solid #4d90fe;
 		border-left: 0;
@@ -33,26 +36,29 @@
 	div#container-up #videoList,
 	div#container-up #imageList,
 	div#container-up #urlUpload{ display: none; }
-	#tag-multime div.active{ font-weight: bold; }
+	.upload-menu div.active{ font-weight: bold; }
 	div#container-up.up #fileupload,
 	div#container-up.vid #videoList,
 	div#container-up.alt #imageList,
 	div#container-up.img #urlUpload{ display: block !important; }
+	div.upload.container{
+		width:750px;
+		height:350px;
+		overflow-x:auto;
+	}
+	.col-lg-7{
+		width:100%;
+	}
 </style>
-<div class="container">	
-	<div id="urlUpload">
-		<div id="videosTag">
-			<label><?=NEWTAG_LBLVIDEO?>:</label><br>
-			<input style="text-align: left; width: 370px;height: 16px;" type="text" name="txtVideo" id="txtVideo" class="tag-text" requerido="video" tipo="video" value="<?=$tag['video_url']?$tag['video_url']:'http://'?>" placeholder="http://" <?php if(NEWTAG_LBLVIDEO_TITLE!=""){?> title="<?=NEWTAG_LBLVIDEO_TITLE?>" <?php } else{}?>/>
-			<div id="vimeo">
-				<div id="running" class="warning-box dnone"><?=VIMEO_PREMIUM_VERIFY?><span class="loader"></span></div>
-				<div id="success" class="warning-box dnone"><?=VIMEO_PREMIUM_SUCCESS?></div>
-				<div id="error" class="error-box dnone"><?=VIMEO_PREMIUM_DAMAGED?></div>
-			</div>
-		</div>
-	</div>
-	<div class="clearfix"></div>
-	<!-- The file upload form used as target for the file upload widget -->
+<div class="upload-panel">
+<div class="upload-menu">
+	<div data-container="#fileupload" class="active"><?=$lang->get('Upload file')?></div>
+	<div data-container="#videoLink"><?=$lang->get('Youtube/Vimeo Videos')?></div>
+	<div data-container="#imageList"><?=$lang->get('Backgrounds')?></div>
+	<div data-container="#videoList"><?=$lang->get('Videos')?></div>
+	<div data-container="#pendingVideoList"><?=$lang->get('Pending Videos')?></div>
+</div>
+<div class="upload container">
 	<form id="fileupload" action="//jquery-file-upload.appspot.com/" method="POST" enctype="multipart/form-data">
 		<!-- Redirect browsers with JavaScript disabled to the origin page -->
 		<noscript><input type="hidden" name="redirect" value="http://blueimp.github.io/jQuery-File-Upload/"></noscript>
@@ -62,20 +68,20 @@
 				<!-- The fileinput-button span is used to style the file input field as button -->
 				<span class="btn btn-success fileinput-button">
 					<i class="glyphicon glyphicon-plus"></i>
-					<span>Add files...</span>
+					<span><?=$lang->get('Add files')?>...</span>
 					<input type="file" name="files[]" multiple>
 				</span>
 				<button type="submit" class="btn btn-primary start">
 					<i class="glyphicon glyphicon-upload"></i>
-					<span>Start upload</span>
+					<span><?=$lang->get('Start upload')?></span>
 				</button>
 				<button type="reset" class="btn btn-warning cancel">
 					<i class="glyphicon glyphicon-ban-circle"></i>
-					<span>Cancel upload</span>
+					<span><?=$lang->get('Cancel upload')?></span>
 				</button>
 				<button type="button" class="btn btn-danger delete">
 					<i class="glyphicon glyphicon-trash"></i>
-					<span>Delete</span>
+					<span><?=$lang->get('Delete')?></span>
 				</button>
 				<input type="checkbox" class="toggle">
 				<!-- The global file processing state -->
@@ -94,19 +100,36 @@
 		<!-- The table listing the files available for upload/download -->
 		<table role="presentation" class="table table-striped"><tbody class="files"></tbody></table>
 	</form>
-	<!-- The file upload form used as target for the file upload widget -->
-	<form id="videoList" action="//jquery-file-upload.appspot.com/" method="POST" enctype="multipart/form-data">
-		<h3>Videos</h3>
-		<!-- The table listing the files available for upload/download -->
-		<table role="presentation" class="table table-striped"><tbody class="files"></tbody></table>
-	</form>
-	<form id="imageList" action="//jquery-file-upload.appspot.com/" method="POST" enctype="multipart/form-data">
+	<div id="videoLink" class="dnone">
+		<div id="videosTag">
+			<label><?=$lang->get('Video Link')?>:</label><br>
+			<input style="text-align:left;width:370px;height:16px;" type="text" name="txtVideo" id="txtVideo" class="tag-text" requerido="video" tipo="video" value="<?=$tag['video_url']?$tag['video_url']:'http://'?>" placeholder="http://"<?php if($lang->get('NEWTAG_LBLVIDEO_TITLE')!=""){?> title="<?=$lang->get('NEWTAG_LBLVIDEO_TITLE')?>" <?php }else{}?>/>
+			<div id="vimeo">
+				<div id="running" class="warning-box dnone"><?=$lang->get('VIMEO_PREMIUM_VERIFY')?><span class="loader"></span></div>
+				<div id="success" class="warning-box dnone"><?=$lang->get('VIMEO_PREMIUM_SUCCESS')?></div>
+				<div id="error" class="error-box dnone"><?=$lang->get('VIMEO_PREMIUM_DAMAGED')?></div>
+			</div>
+		</div>
+	</div>
+	<form id="imageList" class="dnone" action="//jquery-file-upload.appspot.com/" method="POST" enctype="multipart/form-data">
 		<h3>Images</h3>
 		<!-- The table listing the files available for upload/download -->
 		<table role="presentation" class="table table-striped"><tbody class="files"></tbody></table>
 	</form>
-	<br>
+	<form id="videoList" class="dnone" action="//jquery-file-upload.appspot.com/" method="POST" enctype="multipart/form-data">
+		<h3>Videos</h3>
+		<!-- The table listing the files available for upload/download -->
+		<table role="presentation" class="table table-striped"><tbody class="files"></tbody></table>
+	</form>
+	<form id="pendingVideoList" class="dnone" action="//jquery-file-upload.appspot.com/" method="POST" enctype="multipart/form-data">
+		<h3><?=$lang->get('Pending Videos')?></h3>
+		<p><?=$lang->get('PENDING_VIDEO_INFO')?><p>
+		<!-- The table listing the files available for upload/download -->
+		<table role="presentation" class="table table-striped"><tbody class="files"></tbody></table>
+	</form>
 </div>
+</div>
+
 <!-- The jQuery UI widget factory, can be omitted if jQuery UI is already included -->
 <script src="js/fileupload/vendor/jquery.ui.widget.js"></script>
 <!-- The Load Image plugin is included for the preview images and image resizing functionality -->
@@ -122,6 +145,7 @@
 <!-- The File Upload processing plugin -->
 <script src="js/fileupload/jquery.fileupload-process.js"></script>
 <!-- The File Upload video preview plugin -->
+<script src="js/fileupload/jquery.fileupload-image.js"></script>
 <script src="js/fileupload/jquery.fileupload-video.js"></script>
 <!-- The File Upload validation plugin -->
 <script src="js/fileupload/jquery.fileupload-validate.js"></script>
@@ -132,21 +156,27 @@
 /*global window, $ */
 $(function(){
 	'use strict';
+	//menu
+	$('.upload-menu>div').click(function(event){
+		$(this).parent().children().removeClass('active');
+		$(this).addClass('active');
+		$('.upload.container').children().addClass('dnone')
+		.filter(this.dataset.container).removeClass('dnone');
+	});
 
 	var all_supported=/(\.|\/)(jpe?g|gif|png|mp4|flv|3gp|mov|ogg)$/i,
 		img_supported=/(\.|\/)(jpe?g|gif|png)$/i,
 		video={
 			url:'<?=$setting->video_server_path?>',
 			data:{code:'<?=$client->code?>'},
+			pending:{code:'<?=$client->code?>',folder:'pending'},
 		},
 		img={
 			url:'<?=$setting->img_server_path?>',
 			data:{code:'<?=$client->code?>',folder:'templates'},
 		};
-	//Initialize the jQuery File Upload widget:
+	//File upload form
 	$('#fileupload').fileupload({
-		//Uncomment the following to send cross-domain cookies:
-		//xhrFields: {withCredentials: true},
 		<?php //definimos la url antes de hacer submit, ya que manejamos varios servidores ?>
 		beforeSubmit:function(file,options){
 			if(file.type.match(img_supported)){
@@ -156,30 +186,22 @@ $(function(){
 			}else{
 				//servidor de videos
 				options.url=video.url;
-				options.formData=video.data;
+				options.formData=video.pending;
 			}
 		},
 		acceptFileTypes:all_supported,
 		maxFileSize:15000000,//15MB
-	});
-	// $('#fileuploadVideos').fileupload('option','url',url);
-	// console.log($('#fileuploadVideos').fileupload('option'));
-	//Load existing files:
-	// $('#fileupload').addClass('fileupload-processing');
-	//lista de videos
-	$('#videoList').fileupload();
-	$('#tag-multime div').click(function(event) {
-		var id=$(this).attr('id').split('-');
-		$(this).parent().addClass(id).find('div').removeClass('active');
-		$(this).addClass('active');
-	});
-	$.ajax({
 		//Uncomment the following to send cross-domain cookies:
 		//xhrFields: {withCredentials: true},
-		url:video.url,
+	});
+	//lista de imagenes
+	$.ajax({
+		context:$('#imageList').first().fileupload(),
+		url:img.url,
+		//Uncomment the following to send cross-domain cookies:
+		//xhrFields: {withCredentials: true},
 		dataType:'json',
-		data:video.data,
-		context:$('#videoList')[0]
+		data:img.data
 	}).always(function(){
 		$(this).removeClass('fileupload-processing');
 	}).done(function(result){
@@ -187,17 +209,31 @@ $(function(){
 			.call(this,$.Event('done'),{result:result});
 	});
 	//lista de videos
-	$('#imageList').fileupload();
 	$.ajax({
+		context:$('#videoList').first().fileupload(),
+		url:video.url,
 		//Uncomment the following to send cross-domain cookies:
 		//xhrFields: {withCredentials: true},
-		url:img.url,
 		dataType:'json',
-		data:img.data,
-		context:$('#imageList')[0]
+		data:video.data
 	}).always(function(){
 		$(this).removeClass('fileupload-processing');
 	}).done(function(result){
+		$(this).fileupload('option','done')
+			.call(this,$.Event('done'),{result:result});
+	});
+	//lista de videos pendientes
+	$.ajax({
+		context:$('#pendingVideoList').first().fileupload(),
+		url:video.url,
+		//Uncomment the following to send cross-domain cookies:
+		//xhrFields: {withCredentials: true},
+		dataType:'json',
+		data:video.pending
+	}).always(function(){
+		$(this).removeClass('fileupload-processing');
+	}).done(function(result){
+		console.log('pendig videos:',result);
 		$(this).fileupload('option','done')
 			.call(this,$.Event('done'),{result:result});
 	});
@@ -206,30 +242,28 @@ $(function(){
 <!-- Teplates -->
 <!-- The template to display files available for upload -->
 <script id="template-upload" type="text/x-tmpl">
-{% for (var i=0, file; file=o.files[i]; i++) { %}
+{% console.log(o.files); %}
+{% for(var i=0,file;file=o.files[i];i++){ %}
 	<tr class="template-upload fade">
-		<td>
+		<td style="width:50%;">
 			<span class="preview"></span>
-		</td>
-		<td>
-			<p class="name">{%=file.name%}</p>
 			<strong class="error text-danger"></strong>
 		</td>
 		<td>
 			<p class="size">Processing...</p>
 			<div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0"><div class="progress-bar progress-bar-success" style="width:0%;"></div></div>
 		</td>
-		<td>
+		<td style="width:25%;">
 			{% if (!i && !o.options.autoUpload) { %}
 				<button class="btn btn-primary start" disabled>
 					<i class="glyphicon glyphicon-upload"></i>
-					<span>Start</span>
+					<span><?=$lang->get('Start')?></span>
 				</button>
 			{% } %}
 			{% if (!i) { %}
 				<button class="btn btn-warning cancel">
 					<i class="glyphicon glyphicon-ban-circle"></i>
-					<span>Cancel</span>
+					<span><?=$lang->get('Cancel')?></span>
 				</button>
 			{% } %}
 		</td>
@@ -247,27 +281,21 @@ $(function(){
 }; %}
 {% for(var i=0,file; file=o.files[i]; i++){ %}
 	<tr class="template-download fade">
-		<td>
-			<span class="preview">
+		<td style="width:50%;">
+			<span class="preview" action="tag-template" data-url="{%=file.url%}">
 				{% if(file.thumbnailUrl){ %}
 					<a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" data-gallery><img src="{%=file.thumbnailUrl%}"></a>
+				{% }else if(file.url.match(/\.mp4$/i)){ %}
+					<video src="{%=file.url%}" controls></video>
 				{% } %}
+			<strong class="error text-danger"></strong>
 			</span>
-		</td>
-		<td>
-			<p class="name">
-				{% if(file.url){ %}
-					<a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" {%=file.thumbnailUrl?'data-gallery':''%}>{%=file.name%}</a>
-				{% }else{ %}
-					<span>{%=file.name%}</span>
-				{% } %}
-			</p>
 			{% if(file.error){ %}
 				<div><span class="label label-danger">Error</span> {%=file.error%}</div>
 			{% } %}
 		</td>
-		<td>
-			<span class="size">{%=o.formatFileSize(file.size)%}</span>
+		<td style="width:25%;">
+			<span class="size"></span>
 		</td>
 		<td>
 			{% if(file.deleteUrl){ %}
@@ -275,13 +303,13 @@ $(function(){
 					{% if(file.deleteWithCredentials){ %} data-xhr-fields='{"withCredentials":true}'{% } %}
 					data-url="{%=file.deleteUrl+o.get(file.name)%}">
 					<i class="glyphicon glyphicon-trash"></i>
-					<span>Delete</span>
+					<span><?=$lang->get('Delete')?></span>
 				</button>
 				<input type="checkbox" name="delete" value="1" class="toggle">
 			{% }else{ %}
 				<button class="btn btn-warning cancel">
 					<i class="glyphicon glyphicon-ban-circle"></i>
-					<span>Cancel</span>
+					<span><?=$lang->get('Cancel')?></span>
 				</button>
 			{% } %}
 		</td>
