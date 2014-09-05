75be6c6957fc4790a2db0cbb32154710cbcfd0a2
wffranco <willemfranco@gmail.com>
2014-09-05 11:53:19 -0430

    is_assoc (verificar si un arreglo es asociativo)
    Former-commit-id: 3e95f9f56e57241550d6e55f16485104eaf756e0

--- main/core/globals.php
+++ main/core/globals.php
@@ -3,3 +3,8 @@
 function base_url($url=''){
 	return $url;
 }
+
+function is_assoc($a){#detecta si un arreglo es asosiativo
+	if(!is_array($a)) return false;
+    return !!count(array_diff(array_keys($a),range(0,count($a)-1)));
+}
