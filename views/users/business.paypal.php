9435a8aa29d2d31f89cf4a8ebcce02ab511f8100
ramonJR <maxtri@hotmail.com>
2014-09-22 16:13:04 -0430

    business plans, validation paypal
    Former-commit-id: 98105b5437d37df0125a77cf3d6aff6dea19527b [formerly 9f10114d6b0d27943987f0f9c8f4177b8fa02d81]
    Former-commit-id: abacd41b886101ede3a4aa821f5ada89e949ff51

--- views/users/business.paypal.php
+++ views/users/business.paypal.php
@@ -3,85 +3,61 @@ if (!isset( $_SESSION['business_payment'] ) || $_GET['uid']==''){
 	redirect('index.php');
 	die();
 }
-$query  = 'SELECT u.id AS id, u.profile_image_url AS photo, u.email, 
-			CONCAT(name," ",last_name) AS full_name, MD5(CONCAT(u.id,\'_\',u.email,\'_\',u.id)) AS code
-			FROM users u 
-			-- INNER JOIN users_plan_purchase p ON u.id = p.id_user
-			WHERE md5(concat(u.id,\'_\',u.email,\'_\',u.id))="'.$_GET['uid'].'" LIMIT 1';
-$result = $GLOBALS['cn']->query($query);
-$result = mysql_fetch_assoc($result);
+// datos del usuario
+$result=CON::getRow("SELECT u.id AS id, u.profile_image_url AS photo, u.email,
+						CONCAT(name,' ',last_name) AS full_name, 
+						MD5(CONCAT(u.id,'_',u.email,'_',u.id)) AS code
+ 						FROM users u 
+			 			WHERE md5(concat(u.id,'_',u.email,'_',u.id))=? LIMIT 1",array($_GET['uid']));
 $fot_	= FILESERVER.getUserPicture($_GET['uid'].'/'.$result['photo'],'img/users/default.png');
-$userId = md5($result['id']);
 
-//Costo de suscipcion 
-$subcriptionPrice = $GLOBALS['cn']->queryRow('SELECT cost_account_company FROM config_system WHERE id = 1 LIMIT 1;');
-$subcriptionPrice = $subcriptionPrice['cost_account_company'];
-
-//Verifica si ya a pagado suscripción tiene un plan
-$plan = 3;
-$query = "SELECT id_plan FROM users_plan_purchase WHERE id_user='".$result['id']."' AND id_plan > 0 ORDER BY id DESC LIMIT 1;";
-$userPlan = $GLOBALS['cn']->query($query);
-if(mysql_num_rows($userPlan) > 0){
-	$userPlan = mysql_fetch_assoc($userPlan);
-	$subcriptionPrice = 0;
-	$plan = $userPlan['id_plan'];
+//plan por defecto y costo de la inscripcion
+$plan=CON::getVal("SELECT id_plan FROM users_plan_purchase 
+				WHERE id_user=? ORDER BY id DESC LIMIT 1;",array($result['id']));
+if ($plan!==null) $subcriptionPrice = 0;
+else{
+	$plan=1;
+	$subcriptionPrice=CON::getVal('SELECT cost_account_company FROM config_system WHERE id = 1 LIMIT 1;');
 }
 
 //Tipos de cuentas business.
-$query = 'SELECT *, p.id AS idPlan FROM subscription_plans p
-		  INNER JOIN subscription_plans_detail d ON p.id=d.id_plan
-		  WHERE p.status = 1 ORDER BY p.id DESC';
-$types=$GLOBALS['cn']->query($query);
+$query=CON::query("	SELECT *, p.id AS idPlan FROM subscription_plans p
+			 		INNER JOIN subscription_plans_detail d ON p.id=d.id_plan
+ 		  			WHERE p.status = 1 ORDER BY p.id DESC");
 ?>
 <container id="paypal-help" class="bg cache">
 	<content>
 	<div class="pay-help ui-single-box">
-		<div class="ui-single-box-title">
-			<?=SIGNUP_SELECTPLANTITLE?><br>
-		</div>
+		<div class="ui-single-box-title"><?=SIGNUP_SELECTPLANTITLE?></div>
 		<div class="pay-help-content">
 			<div class="user-info">
 				<div class="bg"><img src="<?=$fot_?>" width="60" height="60"></div>
 				<div class="info">
-					<strong><?=  formatoCadena($result['full_name'])?></strong>
+					<strong><?=formatoCadena($result['full_name'])?></strong>
 					<br><span><?=$result['email']?></span>
 				</div>
 			</div>
 			<form action="controls/users/nonprofitDocuments.php" method="POST" enctype="multipart/form-data" id="frmBusiness">
 				<div class="account-types">
 					<h4><?=SIGNUP_PLANS_INFO?></h4>
-					<em>
-						<?=MSG_PAYPAL_HELP?>
-					</em>
+					<em><?=MSG_PAYPAL_HELP?></em>
 					<ul class="plans clearfix">
 					<?php
-					while ( $type=mysql_fetch_assoc($types) ):
+					while ( $type=CON::fetchAssoc($query) ):
 						//Verifica si los planes tienen traduccion
 						$name = (defined($type['name'])) ? constant($type['name']) : $type['name'] ;
 						$description = (defined($type['description'])) ? constant($type['description']) : $type['description'] ;
-
 						//Checkeaa y activa un plan previamente comprado o selecciona el 1ero por defecto
 						$selected = ( $type['idPlan'] == $plan ) ? 'checked="checked"' : '';
-
 						$features = explode(',', $type['features']);
-
 						//Dias de la suscripcion
 						switch ($type['days']) {
-							case 30:
-								$subDays = TOPTAGS_MONTHLY;
-							break;
-							case 36:
-								$subDays = TOPTAGS_QUARTERLY;
-							break;
-							case 365:
-								$subDays = TOPTAGS_YEARLY;
-							break;
-							case 0:
-								$subDays = SIGNUP_UNLIMITEDLBL;
-							break;
+							case 30: $subDays = TOPTAGS_MONTHLY; break;
+							case 36: $subDays = TOPTAGS_QUARTERLY; break; 
+							case 365: $subDays = TOPTAGS_YEARLY; break;
+							case 0: $subDays = SIGNUP_UNLIMITEDLBL; break;
 						}
 					?>
-
 						<label>
 							<li class="content-plan" title="<?=$type['description']?>">
 								<h3>
@@ -104,9 +80,7 @@ $types=$GLOBALS['cn']->query($query);
 								<div id="price<?=$type['idPlan']?>" class="price color_green"><?=($type['price'] <= 0) ? SIGNUP_EXONERATEDLBL : $type['price'] ;?></div>
 							</li>
 						</label>
-					<?php
-					endwhile;
-					?>
+					<?php endwhile; ?>
 					</ul>
 					<div id="calculator">
 						<div class="hr-double"></div>
@@ -129,18 +103,13 @@ $types=$GLOBALS['cn']->query($query);
 								for ($i=0;$i<5;$i++){
 									echo '
 										<div id="photoUpload'.($i+1).'" class="frmProfileBotones">
-											<!--div id="photoDIVS-'.($i+1).'"></div-->
-											<div class="prevPhotoStore">
-												<div n="'.($i+1).'" title="'.NEWTAG_UPLOADBACKGROUND.'" class="bgS'.($i+1).'" '.(($photos[($i+1)])?'style="background-image: url(\''.FILESERVER."img/".$photos[($i+1)]['photo'].'\');background-size:100%;"':'').'>
+											<div class="prevPhotoStore" style="width:87px;">
+												<div n="'.($i+1).'" title="'.NEWTAG_UPLOADBACKGROUND.'" class="bgS'.($i+1).'" >
 												</div>
 												<img id="loaderStore'.($i+1).'" style="display:none;position: relative;top:-25px;" src="img/loader.gif" width="25" height="25" />
 											</div>
-											<div id="photoDIV'.($i+1).'" class="invisible"><input accept="image/jpg,image/jpeg,image/png,image/bmp" type="file" id="photo'.($i+1).'" class="invisible" name="document[]" value="'.(($_GET[idProd])?$product['photo']:'').'" '.(($_GET[idProd])?'':'requerido="'.PRODUCTS_PICTURE.'"').'/></div>
-											<input type="hidden" id="backgSelect_'.($i+1).'" name="backgSelect_'.($i+1).'" value="'.(($photos[($i+1)])?$photos[($i+1)]['photo']:'').'"/>
-											<span id="photoSpan'.($i+1).'" class="photoSpan"></span>
-											<!--span>'.GROUPS_NEWPHOTO.' ('.($i+1).'): '.STORE_ORDER.'</span-->
-											<input name="txtOrder'.($i+1).'" type="hidden" id="txtOrder'.($i+1).'" size="3" maxlength="4"
-											class="txt_box" value="'.($i+1).'" requerido="'.STORE_ORDER.' '.GROUPS_NEWPHOTO.' ('.($i+1).')"/>
+											<div id="photoDIV'.($i+1).'" class="invisible"><input accept="image/jpg,image/jpeg,image/png,image/bmp" type="file" id="photo'.($i+1).'" class="invisible" name="document[]" requerido="'.SIGNUP_DOCSLBL.'"/></div>
+											<input type="hidden" id="backgSelect_'.($i+1).'" name="backgSelect_'.($i+1).'" value=""/>
 										</div>
 									';
 								}
@@ -157,13 +126,10 @@ $types=$GLOBALS['cn']->query($query);
 	</div>
 	</content>
 </container>
-
 <script>
 var subcriptionPrice = <?=$subcriptionPrice?>;
-
 $(function() {
 delete $.fullpage;
-
 $.on({
 	open:function(){
 		$('footer').hide();
@@ -246,10 +212,6 @@ $.on({
 		}else{
 			$('div.bgS'+item).css('background-image','url(css/smt/document.png)');
 		} 
-
-		// activa el boton continuar si hay al menos un archivo que subir
-//		if(isValidInput) $('#btnPay').button('enable');
-//		else $('#btnPay').button('disable');
 	});
 
 	// Levanta cuadro de dialogo subir documento
@@ -265,67 +227,68 @@ $.on({
 	//Accion al enviar el formulario a paypal
 	$$('#btnPay').click(function(e){
 		var selectedPlan = $('.account-types input[name=plan]:checked').val();
-
 		if(selectedPlan != 0 ){
 			e.preventDefault();
 			e.stopPropagation();
-			document.location = "views/pay.view.php?uid="+'<?=$userId?>'+'&plan='+selectedPlan;
+			document.location = "views/pay.view.php?uid=<?=md5($result['id'])?>"+'&plan='+selectedPlan;
 			return false;
-		}
-
-		$("input.invisible[id^='photo']").each(function(index, el) {
-			if( $(el).val() != '' ) $('#loaderStore'+(index+1)).css('display','block');
-		});
-
-		//Mando Formulario en caso de cuenta NoN-Profit
-		$$('#frmBusiness').ajaxSubmit({
-			dataType: 'json',
-			success: function(data){ // post-submit callback
-				// console.log('La data es:'+data);
-				var msgTitle = 'Error';
-				var msgContent = '';
-				switch(data.result){
-					case -1:
-						msgContent = '<?=SIGNUP_ERRORUPLOAD?>';
-					break;
-					case 0:
-						msgContent = '<?=SIGNUP_ERRORREGISTER?>'
-					break;
-					case 1:
-						msgTitle = '<?=JS_ALERT?>';
-						msgContent = '<?=SIGNUP_SUCCESSREGISTERNONPROFIT?>';
-					break;
+		}else{
+			var band='<?=(($plan!==null && $plan==0)?"true":"false")?>';
+			band=band=='true'?true:false;
+			$("input.invisible[id^='photo']").each(function(index, el) {
+				if( $(el).val() != '' ){
+					$('#loaderStore'+(index+1)).css('display','block');
+					band=true;
 				}
-
-				if( data.result == 1) {
-					$.dialog({
-						title:msgTitle,
-						resizable:false,
-						width:320,
-						height:300,
-						modal:true,
-						open:function(){
-							$(this).html(msgContent);
-						},
-						close:function(){
-							document.location='';
-						},
-						buttons:{
-							'<?=JS_OK?>':function(){
-								$(this).dialog('close');
-							}
+			});
+			if (band){
+				//Mando Formulario en caso de cuenta NoN-Profit
+				$('#frmBusiness').ajaxSubmit({
+					dataType: 'json',
+					success: function(data){ // post-submit callback
+						var msgTitle = 'Error',msgContent = '',location='';
+						switch(data.result){
+							case -1: 
+								msgContent = '<?=SIGNUP_ERRORUPLOAD?>'; 
+								location=document.location;
+							break;
+							case 0: msgContent = '<?=SIGNUP_ERRORREGISTER?>'; break;
+							case 1:
+								msgTitle = '<?=JS_ALERT?>';
+								msgContent = '<?=SIGNUP_SUCCESSREGISTERNONPROFIT?>';
+							break;
 						}
-					});
-				}else{
-					$('div.bgS'+item).css('background-image','url(css/smt/document.png)');
-				}
-			},//success
-			error: function(data){
-				message('<?=NEWTAG_LBLTEXT?>','Error','<?=JS_ERROR?>','','','','','');
-			}
-		});
+						if( data.result == 1 || data.result == -1) {
+							$.dialog({
+								title:msgTitle,
+								resizable:false,
+								width:320,
+								height:300,
+								modal:true,
+								open:function(){ $(this).html(msgContent); },
+								close:function(){ document.location=location; },
+								buttons:{
+									'<?=JS_OK?>':function(){
+										$(this).dialog('close');
+									}
+								}
+							});
+						}else{
+							for (var i=0; i<5; i++) $('#loaderStore'+(i+1)).css('display','block'); 
+							// $('div.bgS'+item).css('background-image','url(css/smt/document.png)'); 
+						}
+					},//success
+					error: function(data){
+						message('<?=NEWTAG_LBLTEXT?>','Error','<?=JS_ERROR?>','','','','','');
+					}
+				});
+			}else{ message('<?=NEWTAG_LBLTEXT?>','Error','<?=$lang["NODOCUMENTSNONPROFIT"]!=""?$lang["NODOCUMENTSNONPROFIT"]:"Please enter the required documents to verify your account"?>','','','','',''); }
+		}
 	});
 	$('.price, .total-details span').formatCurrency();
 	$('#actualUrl').attr('value',document.location.href); //fix
+	<?php if ($plan!==null && $plan==0){ ?>
+	 $('.content-plan input[type=radio]').change();
+	 <?php } ?>
 }); //Document ready
 </script>
