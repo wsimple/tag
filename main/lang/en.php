057047684e8a690b02ec27c71cace3b1cc9649df
wffranco <willemfranco@gmail.com>
2014-10-03 14:38:21 -0430

    movidas algunas definisiones a las traducciones
    Former-commit-id: 55e8dc55bb2dc526dde534ca76485e957e57a371

--- main/lang/en.php
+++ main/lang/en.php
@@ -1,7 +1,16 @@
 <?php
 $lang=array(
+	#Basicas
+	'OK'=>'Ok',
 	#variables de la web
+	'AUTHOR'=>'Websarrollo.com, Maoghost.com',
+	'COPYRIGHT'=>'Tagamation, LLc',
 	'COPYFOOTER'=>'<strong>&copy; 2011 - '.date('Y').' Tagamation</strong>, LLC Patent Pending',
+	'DESCRIPTION'=>'Tagbum is the right site to share information, ideas, opinions and thoughts, using short texts, pictures and videos; A multimedia social network, where you can express what you think at the speed of your imagination.',
+	'KEYWORDS'=>"Winning,place for U,It's time to winning,tag,tags,imagination,oftag,urtag,yourtag,geturtag,play,game,business,personal",
+	'EMAIL_CONTACTO'=>'contact@tagbum.com',
+	'EMAIL_NO_RESPONDA'=>'no-reply@tagbum.com',
+	'PERSONA_CONTACTO'=>'The team Tagbum',
 	#otras variables
 	'HREF_DEFAULT' => 'javascript:void(0)',
 	'TITLE' => "Tagbum.com - It's Time",
