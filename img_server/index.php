81e75d3d91076a8eba13c2a4aed551b77f4c0287
wffranco <willemfranco@gmail.com>
2014-10-28 14:01:50 -0430

    autoload en servidor de imagenes
    Former-commit-id: f1e0634d33a2018f927ad7df4a7c5633bbda5f2e [formerly 55c921d9f15614174474c8b6c56b478003d84578]
    Former-commit-id: a14a3c51119efa034b9ebb6d272501e0ab736312

--- img_server/index.php
+++ img_server/index.php
@@ -13,5 +13,11 @@ error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors',1);
 require_once('includes/client.php');
 
-require('UploadHandler.php');
+function __autoload($classname){
+	#carga automatica de clases permitidas
+	$file="$classname.php";
+	if(is_file($file)) require_once $file;
+	else die('Class not found.');
+}
+
 $upload_handler = new UploadHandler();
