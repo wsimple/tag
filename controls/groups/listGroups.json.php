da9012fa7d0e4e71c5188752f9a7392ca4ad2be0
elibo <elijose.c@gmail.com>
2014-10-30 14:54:19 -0430

    Pequeño arreglo Etiquetas Categoria en Grupos
    Former-commit-id: 92d31343374016c3daf6ca1bd1580300daf93b79 [formerly dc25b9c245ad948f9937d7f30a29d8bc552aaf13]
    Former-commit-id: 067c3d410117ca5710771eb80a85b25d84ecc2e3

--- controls/groups/listGroups.json.php
+++ controls/groups/listGroups.json.php
@@ -141,7 +141,8 @@
                     $row['photo']= $photo!=''? 'style="background-image:url(\''.$photo.'\');"':'';
                     $row['cname']=formatoCadena(lan($row['cname']));
                     $row['photoi']= $photo!=''? 'src="'.$photo.'" ':'src="'.DOMINIO.'css/smt/groups_default.png"';
-                    if ($mobile){ $row['cate_name']=lan('Category').': '.($row['cname'] == '')?'General':$row['cname']; }
+                    $cate_name = ($row['cname'] == '' || strpos($row['cname'], 'GROUPS_CATEGORY_')>= 0 )?'General':$row['cname'];
+                    if ($mobile){ $row['cate_name']=__('Category').': '.$cate_name; }
                     $row['name']=  formatoCadena($row['name']);
                     $row['des']=  formatoCadena($row['des']);
                     $row['members']=  $row['num_members'];
