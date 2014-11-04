cafc5e62420ee4bdf7ca8ff52b25c3994e9d8d39
elibo <elijose.c@gmail.com>
2014-11-04 10:15:44 -0430

    Correccion: Redireccion al crear nueva tag
    Former-commit-id: 135070dd2fa40b0b115eb6aa44ea27b9a8002d6f

--- app/newtag.php
+++ app/newtag.php
@@ -375,6 +375,7 @@
 						},
 						success:function(data) {
 							if(data['done']){
+								localStorage.removeItem('timeLine');
 								if(false&&$.local('enableLogs')){
 									myDialog({
 										id:'#tagUploadDialog',
@@ -418,7 +419,6 @@
 								content:data,
 								buttons:{
 									Ok:function(){
-										localStorage.removeItem('timeLine');
 										redir(PAGE['timeline']);
 									}
 								}
