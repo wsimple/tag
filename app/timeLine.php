d5800a76b2acc6199929fc3e93193f799a04b565
Adrian esqueda <aesqueda14@gmail.com>
2014-11-20 16:38:13 -0430

    reorganizando iconos principal
    Former-commit-id: e2674317e9a599d89b171f8ce3b052087a3710c1 [formerly 7b1b1b525cca490d6442de4dc414777b8c65c68a]
    Former-commit-id: 23303580965f02b0cfc57b79d46662eb1a3b21e0

--- app/timeLine.php
+++ app/timeLine.php
@@ -19,15 +19,18 @@
 <div id="singleRedirDialog" class="myDialog" style="display: none;"><div class="table"><div class="cell"><div class="window" style="max-height: 272px; display: block;"><div class="container" style="max-height: 272px;"><div id="scroller" class="content">Está seguro que quiere eliminar esta tag?</div></div><div class="buttons"><a action="0" class="ui-btn ui-shadow ui-btn-corner-all ui-btn-hover-f ui-btn-up-f ui-btn-up-undefined"><span class="ui-btn-inner ui-btn-corner-all"><span class="ui-btn-text">Sí</span></span></a><a action="1" class="ui-btn ui-shadow ui-btn-corner-all ui-btn-hover-f ui-btn-up-f ui-btn-up-undefined"><span class="ui-btn-inner ui-btn-corner-all"><span class="ui-btn-text">No</span></span></a></div></div></div></div><div class="closedialog" style="display:none"></div></div>
 <div id="page-timeLine" data-role="page" data-cache="false">
 	<div  data-role="header" data-theme="f" data-position="fixed">
-		<div style="position:absolute;top:0px;left:0;padding:0 5px;">
+		<div style="position:absolute;top:0px;left:0;padding:5px 5px;">
 			<!--<a href="<?=base_url('myMenu')?>" style="position:relative;"><span class="btn-menu"></span><span class="push-notifications button" style="display:none;">0</span></a>-->
 			<a href="#" class="showMenu" style="position:relative;"><span class="btn-menu showMenu"></span><span class="push-notifications button" style="display:none;">0</span></a>
-			<a href="#" onclick="redir(PAGE['newtag'])"><img src="css/smt/creation.png" /></a>
+			
 		</div>
 		<h1><span class="loader"></span></h1>
-		<div id="userPoints" class="ui-btn-right" data-iconshadow="true" data-wrapperels="span">
-			<span class="loader"></span>
+		<div style="position:absolute;top:0px;right:0;padding:0 5px;">
+			<a href="#" onclick="redir(PAGE['newtag'])"><img src="css/smt/creation.png" /></a>
 		</div>
+		<<!-- div id="userPoints" class="ui-btn-right" data-iconshadow="true" data-wrapperels="span">
+			<span class="loader"></span>
+		</div> -->
 		<fieldset id="private-select" data-role="controlgroup" data-type="horizontal" data-mini="true" style="position:absolute;top:7px;right:5px;display:none;">
 			<input id="radio-inbox" type="radio" name="radio-in-out" data-theme="a" value="in" checked="checked"/>
 			<input id="radio-outbox" type="radio" name="radio-in-out" data-theme="a" value="out"/>
@@ -57,7 +60,7 @@
 			before:function(){
 				$('.pullDownLabel').html(lan('SCROLL_PULLDOWN'));
 				$('.pullUpLabel').html(lan('SCROLL_PULLUP'));
-				$('#userPoints').html(lan('POINTS_USERS')+' <b><loader/></b>');
+				// $('#userPoints').html(lan('POINTS_USERS')+' <b><loader/></b>');
 				$('#tl-footer ul').html(//llenado de footer
 					'<li><a opc="timeLine" data-icon="tag-tl" data-iconpos="top" class="ui-btn-active">'+lang.JS_TIMELINE+'</a></li>'+
 					'<li><a opc="myTags" data-icon="tag-mt" data-iconpos="top">'+lang.JS_MYTAGS+'</a></li>'+
@@ -75,10 +78,10 @@
 					$('#tl-footer ul li a[opc='+active_tab+']').addClass('ui-btn-active');
 					if(active_tab=='privateTags'){
 						$('#private-select').show();
-						$('#userPoints').hide();
+						// $('#userPoints').hide();
 					}else{
 						$('#private-select').hide();
-						$('#userPoints').show();
+						// $('#userPoints').show();
 					}
 				}
 				var opc={ 
@@ -121,47 +124,47 @@
 						if(opc.current=='privateTags'){
 							opc.get=priv;
 							$('#private-select').show();
-							$('#userPoints').hide();
+							// $('#userPoints').hide();
 						}else{
 //							opc.get='';
 							$('#private-select').hide();
-							$('#userPoints').show();
+							// $('#userPoints').show();
 						}
 						delete opc.on;
 						$wrapper.ptrScroll('reload');
 					}
 				});
-				$('#userPoints').click(function(){
-					myDialog({
-						id:'msg-points',
-						open:true,
-						content:
-							'<p>'+lang.MAINMENU_POINTS_2+'</p>'+
-							'<p>'+lang.MAINMENU_POINTS_1+'</p>',
-						style:{
-							'margin':10,
-							'font-size':14
-						}
-					});
-				});
+				// $('#userPoints').click(function(){
+				// 	myDialog({
+				// 		id:'msg-points',
+				// 		open:true,
+				// 		content:
+				// 			'<p>'+lang.MAINMENU_POINTS_2+'</p>'+
+				// 			'<p>'+lang.MAINMENU_POINTS_1+'</p>',
+				// 		style:{
+				// 			'margin':10,
+				// 			'font-size':14
+				// 		}
+				// 	});
+				// });
 				//Get User Points
-				$.ajax({
-					type	:'GET',
-					url		:DOMINIO+'controls/users/getUserPoints.json.php',
-					dataType:'json',
-					success	:function(data){
-						var datos='',pts='';
-						pts=data.split(' ');
-						//alert(pts[1]);
-						if(pts[1]=='CONST_UNITMIL')
-							datos=pts[0]+' K';
-						else if(pts[1]=='CONST_UNITMILLON')
-							datos=pts[0]+' M';
-						else
-							datos=data;
-						$('#userPoints b').html(datos);
-					}
-				});
+				// $.ajax({
+				// 	type	:'GET',
+				// 	url		:DOMINIO+'controls/users/getUserPoints.json.php',
+				// 	dataType:'json',
+				// 	success	:function(data){
+				// 		var datos='',pts='';
+				// 		pts=data.split(' ');
+				// 		//alert(pts[1]);
+				// 		if(pts[1]=='CONST_UNITMIL')
+				// 			datos=pts[0]+' K';
+				// 		else if(pts[1]=='CONST_UNITMILLON')
+				// 			datos=pts[0]+' M';
+				// 		else
+				// 			datos=data;
+				// 		$('#userPoints b').html(datos);
+				// 	}
+				// });
 			}
 		});
 	</script>
