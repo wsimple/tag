67b06f30e84afc3604a0ee659251d09b673caea4
wffranco <willemfranco@gmail.com>
2014-11-21 16:44:37 -0430

    arreglo plugin youtube
    Former-commit-id: 77a1cbe3d831430f4afdf281fed81b9daea9e88e

--- templates/css_js.php
+++ templates/css_js.php
@@ -32,7 +32,7 @@ if(!ISLOGGED &&'localStorage' in window && window['localStorage']!==null) localS
 <?php if($minify){ ?>
 	<link href="min/?g=css" rel="stylesheet"/>
 	<script src="min/?g=js" charset="utf-8"></script>
-	<script src="http://www.youtube.com/iframe_api"></script>
+	<script src="https://www.youtube.com/iframe_api"></script>
 	<script src="min/?f=js/funciones_<?=$lang['langcode']?>.js" charset="utf-8"></script>
 <?php }else{
 	$cssJsLocal= (require 'min/groupsConfig.php');
