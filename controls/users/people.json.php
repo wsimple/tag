443d6e25a82fdbb31811546b30a852f6b62958ee
jrivas <maxtri@hotmail.com>
2014-11-28 08:30:17 -0430

    leave groups app
    Former-commit-id: ac0ebe3155e4ddcee1fec72e4c9861ba8acc9bd5

--- controls/users/people.json.php
+++ controls/users/people.json.php
@@ -122,10 +122,6 @@ switch ($_GET['action']) {
 	break;
 	case 'groupMembers': //group members
 		if (!isset($_GET['idGroup'])) die(jsonp(array()));	
-		// $coi=isset($_GET['code'])?"md5(concat(id,'_', email,'_',id))":"md5(id)";
-		// if (!isset($_POST['uid']))	$uid=$myId;
-		// else $uid=CON::getVal("SELECT id FROM users WHERE $coi=?",array($_POST['uid']));
-		// if (!$uid) die(jsonp(array('error'=>'noIdValid')));
 		$array['select']=safe_sql(',md5(u.id) AS id_user,g.is_admin,g.status,IF(u.id='.$myId.',1,0) AS iAm,
 						(SELECT oul.id_user FROM users_links oul WHERE oul.id_user=? AND oul.id_friend=u.id) AS conocido,
 						(SELECT COUNT(t.id) FROM tags t WHERE md5(t.id_group)=? AND t.id_creator=g.id_user) AS numTags',array($myId,$_GET['idGroup']));
@@ -133,6 +129,7 @@ switch ($_GET['action']) {
 		$array['order']='ORDER BY g.status,g.date DESC';
 		$array['where']=safe_sql(' md5(g.id_group)=?',array($_GET['idGroup']));
 		if (isset($_GET['status'])) $array['where'].=safe_sql('AND g.status=?',array($_GET['status']));
+		if (isset($_GET['noMy'])) $array['where'].=safe_sql('AND g.id_user!=?',array($myId));
 		$num=CON::query("SELECT g.id FROM users_groups g WHERE ".$array['where']);
 		$res['num']=CON::numRows($num);
 		$query=peoples($array); 
