d8a16996b714675dffa9b6007fd36e20d40a858f
elibo <elijose.c@gmail.com>
2014-12-02 16:01:45 -0430

    Ahora puedes decidir que cantidad vas a mostrar en lista de amigos(Control), Pequeños arreglos de interface, se muestra el numero total de amigos, admirados y admiradores.(APP)
    Former-commit-id: f27eafd66f52dcdfb8c38f9c2bb69695962423fe

--- app/userFriends.php
+++ app/userFriends.php
@@ -33,8 +33,7 @@
 		},
 		after:function(){
 			$('#page-friendUser .ui-btn-inner').css('padding-top',' 5px').css('padding-left', '5px');
-			var opc={layer:'#friendsList',mod:$_GET['type']||'friends',get:"",user:$_GET['id_user']||''},
-			$wrapper=$('#pd-wrapper',this.id);
+			var pag=1,perpag=20,opc={layer:'#friendsList',mod:$_GET['type']||'friends',get:'&offset='+perpag,user:$_GET['id_user']||''},$wrapper=$('#pd-wrapper',this.id);
 
 			$('#friendsFooter li a[opc='+$_GET['type']+']').addClass('ui-btn-active'); //Estilo de li activo
 			// $(opc.layer).wrap('<div class="list-wrapper"><div id="scroller"></div></div>');
@@ -46,20 +45,19 @@
 			$(opc.layer).on('click','[code]',function(){
 				redir(PAGE['profile']+'?id='+$(this).attr('code'));
 			});
-			linkUser(opc.layer);
 
+			linkUser(opc.layer);
 			$(this.id).on('click','.ui-navbar a[opc]',function(){
-				opc.get = null;
+				opc.get='&offset='+perpag;
 				opc.mod=$(this).attr('opc');
 				viewFriends('refresh',opc);
 				$wrapper.jScroll('refresh');
 				$('.list-content input').val('');
 			});
 			viewFriends('refresh',opc);
-			var i = 1, page = 50;
 			$wrapper.ptrScroll({
 				onPullUp:function(){
-					opc.get = '&limit='+(page*i++);
+					opc.get = '&offset='+perpag+'&limit='+(perpag*pag++);
 					var response = viewFriends('more',opc);
 					if (!response) {
 						$wrapper.jScroll('refresh');
