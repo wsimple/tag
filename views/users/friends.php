7a404745cf6d02375606f34db655fc4d81cd778a
wffranco <willemfranco@gmail.com>
2015-02-02 10:37:03 -0430

    busqueda de amigos
    Former-commit-id: 2de8ebb9fe9577a6b2339e44c48e92012487bc9b [formerly e8918c7cd5fc412b6e9b91fbe2099fd26ccdcac9]
    Former-commit-id: f4def438c8d8ffe8b85475d3a0bc7149e822ae30

--- views/users/friends.php
+++ views/users/friends.php
@@ -1,15 +1,17 @@
 <?php
-
+global $section,$params;
 //Seleccion del menu Friends
 $sc = $_GET['sc'];
+if($params[0]!='') $sc=$params[0];
 
 switch ($sc) {
-	case 1: case 2: include("friends/yourFriendsView.php"); break;
+	case 1: case 2: case 'find':
+		include("friends/yourFriendsView.php"); break;
 	// case 2:
 	// 	include("friends/findFriendsView.php");
 	// 	break;
-	case 3: include("friends/inviteFriendsView.php"); break;
+	case 3: case 'invite':
+		include("friends/inviteFriendsView.php"); break;
 	case 4: include('friends/addressBookView.php');	break;
 	default: include("friends/yourFriendsView.php"); break;
 }
-?>
