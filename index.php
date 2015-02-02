39b40f54e8bf37965cc8173996ce133f9782c081
wffranco <willemfranco@gmail.com>
2015-02-02 10:58:52 -0430

    ajuste mvc
    * separado autoload, para uso externo
    
    
    Former-commit-id: ed0b09b0196ec48053ee0cc3f94af713d2444c16 [formerly 3a03ced86d146304828899b875a34d50d4f50a81]
    Former-commit-id: a3381266d045604383d742dd5ac2e01478098872

--- index.php
+++ index.php
@@ -7,29 +7,13 @@ if(substr($_SERVER['SERVER_NAME'],0,4)=='www.'){
 	header('Location: http://'.substr($_SERVER['SERVER_NAME'],4).$_SERVER['REQUEST_URI']);
 	die();
 }
+
 if(isset($_COOKIE['_DEBUG_']))
 	error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT & ~E_NOTICE);
 else
 	error_reporting(0);
-#validamos la existencia del archivo de seguridad
-if(!is_file('.security/security.php')){ include 'security.php'; }
-include('.security/security.php');
-
-#carga automatica de clases permitidas
-function __autoload($classname){
-	$path='main';
-	$folder='controllers';
-	$filename=strtolower($classname);
-	if(strpos($classname,'_model')){ $folder='models'; $filename=str_replace('_model','',$filename); }
-	if(strpos($classname,'_lib')){ $folder='libraries'; $filename=str_replace('_lib','',$filename); }
-	$file="$path/$folder/$filename.php";
-	if(is_file($file)) include_once $file;
-	elseif(is_file("$path/core/$classname.php")) include_once("$path/core/$classname.php");
-	elseif(is_file("$path/plugins/$classname.php")) include_once("$path/plugins/$classname.php");
-	elseif(TAG_functions::is_debug()) die("No existe la clase '$classname' ($folder).");
-	else die('Page not found.');
-}
-
+#precarga de mvc
+include('autoload.php');
 #mobile detection
 APP::detect();
 
