bbf56544423557579381460e104f6b772da2d120
wffranco <willemfranco@gmail.com>
2015-02-12 10:13:20 -0430

    limpieza
    Former-commit-id: 5bc3a0010bb2ec87d64105d709d929e95f00044f [formerly a3afb001555c5be7acd40f4a3086ee809016908d]
    Former-commit-id: b922919087d1027fb444e79aabfae4974bb30e5f

--- controls/news/newsjson.php
+++ controls/news/newsjson.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 	include '../header.json.php';
 	$res=array();
 	$miId=$_SESSION['ws-tags']['ws-user']['id'];
@@ -36,30 +36,32 @@
 	}
 	if ($num){
 		if($limitsql=='') $limitsql="LIMIT 0,250";
-		$new=CON::query("SELECT un.id,
-								un.id_friend,
-								un.id_user,
-								un.id_type,
-								un.id_source,
-								un.date,
-					            t.type_source AS type,
-					            (SELECT CONCAT(u.name,' ',u.last_name) FROM users u WHERE u.id=un.id_user) AS nameUser,
-					            (SELECT CONCAT(u.name,' ',u.last_name) FROM users u WHERE u.id=un.id_friend) AS nameFriend,
-					            (SELECT md5(CONCAT(u.id,'_',u.email,'_',u.id))  FROM users u WHERE u.id=un.id_user) AS keyUser,
-					            (SELECT md5(CONCAT(u.id,'_',u.email,'_',u.id))  FROM users u WHERE u.id=un.id_friend) AS keyFriend,
-					            (SELECT u.profile_image_url FROM users u WHERE u.id=un.id_user) AS photoUser,
-					            (SELECT u.profile_image_url FROM users u WHERE u.id=un.id_friend) AS photoFriend,
-					            IF(un.id_type IN (29,15),(SELECT p.photo FROM store_products p WHERE p.id=un.id_source),
-					            	IF (un.id_type IN (6,12,13),(SELECT g.photo FROM groups g WHERE g.id=un.id_source),NULL)) AS photoS
-							FROM users_notifications un
-							INNER JOIN type_actions t ON un.id_type = t.id
-							WHERE un.id_user!=? AND un.id_friend!=? AND un.id_friend!=0 AND un.id_friend!=un.id_user 
-							AND un.id_type IN (2,4,5,8,9,11,22,25,26,27,29) 
-							".(!$mobile?"AND (un.id_friend IN ($idIn) OR un.id_user IN ($idIn)) AND $undiffDate":"")."
-							ORDER BY un.date DESC $limitsql", array($miId,$miId));
+		$new=CON::query(
+			"SELECT un.id,
+				un.id_friend,
+				un.id_user,
+				un.id_type,
+				un.id_source,
+				un.date,
+				t.type_source AS type,
+				(SELECT CONCAT(u.name,' ',u.last_name) FROM users u WHERE u.id=un.id_user) AS nameUser,
+				(SELECT CONCAT(u.name,' ',u.last_name) FROM users u WHERE u.id=un.id_friend) AS nameFriend,
+				(SELECT md5(CONCAT(u.id,'_',u.email,'_',u.id))  FROM users u WHERE u.id=un.id_user) AS keyUser,
+				(SELECT md5(CONCAT(u.id,'_',u.email,'_',u.id))  FROM users u WHERE u.id=un.id_friend) AS keyFriend,
+				(SELECT u.profile_image_url FROM users u WHERE u.id=un.id_user) AS photoUser,
+				(SELECT u.profile_image_url FROM users u WHERE u.id=un.id_friend) AS photoFriend,
+				IF(un.id_type IN (29,15),(SELECT p.photo FROM store_products p WHERE p.id=un.id_source),
+					IF (un.id_type IN (6,12,13),(SELECT g.photo FROM groups g WHERE g.id=un.id_source),NULL)) AS photoS
+			FROM users_notifications un
+			INNER JOIN type_actions t ON un.id_type = t.id
+			WHERE un.id_user!=? AND un.id_friend!=? AND un.id_friend!=0 AND un.id_friend!=un.id_user
+			AND un.id_type IN (2,4,5,8,9,11,22,25,26,27,29)
+			".(!$mobile?"AND (un.id_friend IN ($idIn) OR un.id_user IN ($idIn)) AND $undiffDate":"")."
+			ORDER BY un.date DESC $limitsql"
+		,array($miId,$miId));
 		// $res['sql-noti']=CON::lastSql();
 		while($row=CON::fetchAssoc($new)){
-			if ($mobile && $_GET['action']!='refresh'){
+			if($mobile && $_GET['action']!='refresh'){
 				$res['numResult']++;
 				if (!isset($infoa) || count($infoa)==0){
 					$res['fecha']=$row['date'];
@@ -67,18 +69,18 @@
 					$res['fecha']=substr($res['fecha'],0,strlen($res['fecha'])-1).$second;
 				}
 			}
-			if (isset($infoa[$row['id_source'].'-'.$row['id_type']])){
+			if(isset($infoa[$row['id_source'].'-'.$row['id_type']])){
 				$band=false;
 				$num=count($infoa[$row['id_source'].'-'.$row['id_type']]['usrs']);
-				for ($i=0;$i<$num; $i++){
+				for($i=0;$i<$num; $i++){
 					if ($infoa[$row['id_source'].'-'.$row['id_type']]['usrs'][$i]['name']===$row['nameUser'])
 						$band=true;
 				}
-				if (!$band){
+				if(!$band){
 					$infoa[$row['id_source'].'-'.$row['id_type']]['usrs'][$num]['name'] = $row['nameUser'];
 					$infoa[$row['id_source'].'-'.$row['id_type']]['usrs'][$num]['uid'] = md5($row['id_user']);
 					$infoa[$row['id_source'].'-'.$row['id_type']]['usrs'][$num]['photo'] = getUserPicture('img/users/'.$row['keyUser'].'/'.$row['photoUser'],false);
-				if (!$infoa[$row['id_source'].'-'.$row['id_type']]['usrs'][$num]['photo']) $infoa[$row['id_source'].'-'.$row['id_type']]['usrs'][$num]['photo']=DOMINIO.'img/users/default.png';
+				if(!$infoa[$row['id_source'].'-'.$row['id_type']]['usrs'][$num]['photo']) $infoa[$row['id_source'].'-'.$row['id_type']]['usrs'][$num]['photo']=DOMINIO.'img/users/default.png';
 				else $infoa[$row['id_source'].'-'.$row['id_type']]['usrs'][$num]['photo']=FILESERVER.$infoa[$row['id_source'].'-'.$row['id_type']]['usrs'][$num]['photo'];
 				}
 			}else{
@@ -106,29 +108,29 @@
 				$afriends[0]=$friend;
 				$row['usrs']=$usr;
 				$row['friend']=$afriends;
-				if ($row['photoS']){
-					if (in_array($row['id_type'],array(29,15))){
+				if($row['photoS']){
+					if(in_array($row['id_type'],array(29,15))){
 						$photo=FILESERVER.'img/'.$row['photoS'];
 						if (fileExistsRemote($photo)) $row['photoS']=$photo;
 						else $row['photoS']=DOMINIO.'imgs/defaultAvatar.png';
 					}
-					if (in_array($row['id_type'],array(6,12,13))){
+					if(in_array($row['id_type'],array(6,12,13))){
 						$photo=FILESERVER.'img/groups/'.$row['photoS'];
 						if (fileExistsRemote($photo)) $row['photoS']=$photo;
 						else $row['photoS']=DOMINIO.'css/smt/groups_default.png"';
 					}
 				}
 				$row['idsource']=$row['id_source'];
-	    		$row['source']=md5($row['id_source']);
+				$row['source']=md5($row['id_source']);
 				$infoa[$row['idsource'].'-'.$row['id_type']]=$row;
 			}
-			if (!$mobile) unset($row['keyUser']);
-			unset($row['date']); unset($row['keyFriend']); 
+			if(!$mobile) unset($row['keyUser']);
+			unset($row['date']); unset($row['keyFriend']);
 			unset($row['nameUser']); unset($row['nameFriend']); unset($row['photoUser']);
 			unset($row['photoFriend']); unset($row['nameFriend']);
-			if ($limit && count($infoa)>=$limit) break;
+			if($limit && count($infoa)>=$limit) break;
 		}
-		if (count($infoa)>0) foreach ($infoa as $key){ $info[]=$key; } 
+		if(count($infoa)>0) foreach($infoa as $key){ $info[]=$key; }
 	}else{ $info=array(); }
 	$res['info']=$info;
 	// echo '<pre>';
@@ -155,19 +157,18 @@
 //		$res['txt']['15']='[_YOU_] commented a [_PROD_] in the store';
 //		$res['txt']['16']='La orden [_ORDER_] ha sido procesada satisfactoriamente';
 //		$res['txt']['17']='La orden [_ORDER_] está pendiente por pagar';
-        $res['txt']['22']='La [_TAG_] de [_PEOPLE_] ha sido la ganadora del d&iacute;a';
-        $res['txt']['25']='La [_TAG_] de [_PEOPLE_] ha sido la ganadora de la semana';
-        $res['txt']['26']='La [_TAG_] de [_PEOPLE_] ha sido la ganadora del mes';
-        $res['txt']['29']='[_PEOPLE_] ha publicado un [_PROD_] en la tienda';
+		$res['txt']['22']='La [_TAG_] de [_PEOPLE_] ha sido la ganadora del d&iacute;a';
+		$res['txt']['25']='La [_TAG_] de [_PEOPLE_] ha sido la ganadora de la semana';
+		$res['txt']['26']='La [_TAG_] de [_PEOPLE_] ha sido la ganadora del mes';
+		$res['txt']['29']='[_PEOPLE_] ha publicado un [_PROD_] en la tienda';
 		$res['txt']['replace']=array(
 			'[_TAG_]'	=>'Tag',
 			'[_AND_]'	=>'y',
 			'[_PROD_]'	=>'producto',
 			'[_MORE_]'	=>'personas m&aacute;s'
 		);
-        
 	break;
-	default: 
+	default:
 //		$res['txt']['1'] ='[_YOU_] receibed a private tag from [_FRIENDS_]';Like the Tag of
 		$res['txt']['2'] ='[_FRIENDS_] Like  the [_TAG_] of [_PEOPLE_]';
 		$res['txt']['4'] ='[_FRIENDS_] comented the [_TAG_] of [_PEOPLE_]';
@@ -184,18 +185,18 @@
 //		$res['txt']['15']='[_YOU_] commented a [_PROD_] in the store';
 //		$res['txt']['16']='The order [_ORDER_] was processed successfully';
 //		$res['txt']['17']='The order [_ORDER_] is pending for payment';
-        $res['txt']['22']='The [_TAG_] to [_PEOPLE_] has been the winner day';
-        $res['txt']['25']='The [_TAG_] to [_PEOPLE_] has been the winner week';
-        $res['txt']['26']='The [_TAG_] to [_PEOPLE_] has been the winner month';
-        $res['txt']['29']='[_PEOPLE_] has published a [_PROD_] in the store';
-        $res['txt']['replace']=array(
+		$res['txt']['22']='The [_TAG_] to [_PEOPLE_] has been the winner day';
+		$res['txt']['25']='The [_TAG_] to [_PEOPLE_] has been the winner week';
+		$res['txt']['26']='The [_TAG_] to [_PEOPLE_] has been the winner month';
+		$res['txt']['29']='[_PEOPLE_] has published a [_PROD_] in the store';
+		$res['txt']['replace']=array(
 			'[_TAG_]'	=>'Tag',
 			'[_AND_]'	=>'and',
 			'[_PROD_]'	=>'product',
 			'[_MORE_]'	=>'more people'
 		);
 	}
-    $res['txtFormat']='function txtFormat(opc){
+	$res['txtFormat']='function txtFormat(opc){
 		var i,num=opc.num||1,info=opc.txt[opc.type],rep=opc.txt["replace"],find,el;
 		if(num>1)
 			info=info.replace(/{{([^}]*)}}/g,"$1").replace(/{([^}]*)}/g, "" );
@@ -210,7 +211,5 @@
 		return info;
 	}';
 	$res['txtFormat']=str_minify($res['txtFormat']);
-	
 
 	die(jsonp($res));
-?>
