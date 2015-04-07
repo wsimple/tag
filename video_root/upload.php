c83ff58dce4b0465fac4bfac3641c2309837c50d
wffranco <willemfranco@gmail.com>
2015-04-07 16:08:27 -0430

    upload para app

--- video_root/upload.php
+++ video_root/upload.php
@@ -4,26 +4,27 @@ if(!$myId){
 	$db=new TAG_db();
 	$user=array();
 	$user=$db->getRow('SELECT id,email FROM users WHERE md5(id)=? AND md5(CONCAT(id,"_",email,"_",id))=?',
-		array($_COOKIE['__uid__'],$_COOKIE['__code__']));
+		[ $_COOKIE['__uid__'],$_COOKIE['__code__'] ]);
 	$myId=$user['id'];
 }
 if(!$myId) $myId=0;
 
-$files=array();
+$folder='/videos/pending';
+$files=[];
 if(count($_FILES)){
 	foreach($_FILES as $file){
-		if($file['error']==0&&rename($file['tmp_name'],"tmp/$myId-".$file['name'])){
-			$files[]="tmp/$myId-".$file['name'];
+		if($file['error']==0){
+			#nombre personalizado
+			$file_name=hash_file('crc32',$file['tmp_name']).'_'.date('YmdHis').'.'.pathinfo($file['name'],PATHINFO_EXTENSION);
+			if(rename($file['tmp_name'],dirname(__FILE__)."$folder/$file_name")){
+				$files[]=['name'=>$file_name];
+			}
 		}
 	}
 }
-
-
-$result_=@move_uploaded_file($_FILES["file"]["tmp_name"], "aaa_".$_FILES["file"]["name"]);
-
-die(json_encode(array(
-
-	'FILES'=>$_FILES,
-	'result'=> $result_,
-	'folder'=> getcwd ()
-)));
+die(json_encode([
+	'files'=>$files,
+	'folder'=> $folder,
+	'request'=> $_REQUEST,
+	'upload'=> $_FILES
+]));
