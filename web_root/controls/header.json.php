ae52ccac707705708b582328e2f846accb9fbf12
Miharbi <miharbihernandez@gmail.com>
2015-05-12 13:52:16 -0500

    debug por pasos

--- web_root/controls/header.json.php
+++ web_root/controls/header.json.php
@@ -17,10 +17,15 @@ if(!$_header_json){
 	$path=preg_replace('/([\/][^\/]*)$/','',str_replace('\\','/',dirname(__FILE__)));
 	@define('RELPATH',"$path/");
 	include_once("$path/includes/config.php");
+	if(is_debug('header')) echo "Time config:".(array_sum(explode(' ',microtime()))-$t)."\n";
 	include "$path/includes/session.php";
+	if(is_debug('header')) echo "Time session:".(array_sum(explode(' ',microtime()))-$t)."\n";
 	include "$path/includes/functions.php";
+	if(is_debug('header')) echo "Time functions:".(array_sum(explode(' ',microtime()))-$t)."\n";
 	include "$path/includes/functions_mails.php";
+	if(is_debug('header')) echo "Time functions_mails:".(array_sum(explode(' ',microtime()))-$t)."\n";
 	include "$path/class/wconecta.class.php";
+	if(is_debug('header')) echo "Time wconecta:".(array_sum(explode(' ',microtime()))-$t)."\n";
 	$_head=array();
 	$_head=apache_request_headers();
 	$mobile=($_POST['CROSSDOMAIN']||$_head['SOURCEFORMAT']=='mobile');
@@ -29,6 +34,9 @@ if(!$_header_json){
 	$myId=$_SESSION['ws-tags']['ws-user']['id'];
 	if(!$myId) $myId=0;
 	if(!$_origin) include RELPATH.'includes/languages.config.php';
+
+	if(is_debug('header')) echo "Time languages:".(array_sum(explode(' ',microtime()))-$t)."\n";
+
 	quitar_inyect();
 	$debug=isset($_REQUEST['debug'])?$_REQUEST['debug']:$_COOKIE['_DEBUG_'];
 	unset($path,$_head);
@@ -36,4 +44,4 @@ if(!$_header_json){
 $_header_json=true;
 if($_need_login&&!$myId) die('');
 
-if(is_debug('header')) echo "Time:".(array_sum(explode(' ',microtime()))-$t)."\n";
+if(is_debug('header')) echo "Time final:".(array_sum(explode(' ',microtime()))-$t)."\n";
