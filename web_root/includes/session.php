d23b5e0bdf490809941a11fb92a6730d2ba76821
wffranco <willemfranco@gmail.com>
2015-05-13 15:11:10 -0430

    funciones de sesion comentadas

--- web_root/includes/session.php
+++ web_root/includes/session.php
@@ -2,21 +2,38 @@
 if($___session___) return;
 $___session___=true;
 
+/**
+ * inicia la sesion y la deja en modo lectura solamente
+ * @return void
+ */
 function load_session(){
 	session_start();
 	session_write_close();
 }
+/**
+ * guarda un arreglo combinandolo con el arreglo guardado en sesion
+ * @param array $data
+ * @return void
+ */
 function save_in_session($data=array()){
 	session_start();
 	$_SESSION=array_merge($_SESSION,$data);
 	session_write_close();
 }
+/**
+ * permite trabajar con la variable de sesion en modo de escritura
+ * @param  function $callable: funcion donde se trabaja con la sesion en modo escritura
+ * -- la funcion puede recibir como parametro el contenido de la sesion
+ * -- si se trabaja con el parametro de la funsion se debe retornar un array.
+ * @return array (opcional): un arreglo que sustituira la sesion actual
+ */
 function with_session($callable){
 	session_start();
 	if(is_callable($callable)) $response=$callable($_SESSION);
 	if(is_array($response)) $_SESSION=$response;
 	session_write_close();
 }
+
 load_session();
 $__val=$_SESSION['ws-tags']['ws-user']['id']!=''?md5($_SESSION['ws-tags']['ws-user']['id']):NULL;
 $__code=$_SESSION['ws-tags']['ws-user']['code']!=''?$_SESSION['ws-tags']['ws-user']['code']:NULL;
@@ -43,4 +60,3 @@ if(	!strpos($_SERVER['PHP_SELF'],'carouselTags.view.php')	&&	!strpos($_SERVER['P
 }
 #regex dominios: /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9]\.[a-zA-Z]{2,}$/
 session_write_close();
-?>
