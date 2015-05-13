104c9938d479d37d3d6649de662d5b7ac8d7a230
wffranco <willemfranco@gmail.com>
2015-05-13 10:37:29 -0430

    VideoUploadApp

--- video_root/upload.php
+++ video_root/upload.php
@@ -29,9 +29,32 @@ if(count($_FILES))
 			}
 		}
 	}
+function __autoload($classname){
+	#carga automatica de clases permitidas
+	$file="$classname.php";
+	if(is_file($file)) require_once $file;
+	else die('Class not found.');
+}
 if(isset($_GET['convert'])){
 	$_REQUEST['file']=$_GET['file']=$_POST['file']=$file_name;
-	include 'index.php';
+	$options=null;
+	if(isset($_SERVER['HTTP_REFERER']))
+		$_referer=$_SERVER['HTTP_REFERER'];
+	elseif(preg_match('/firefox/i',$_SERVER['HTTP_USER_AGENT']))
+		$_referer=$config->main_server;
+	else
+		$_referer='localhost';
+	$_referer=preg_replace('/^(\w+:\/\/)?([^\/]+)(\/.*)?$/','$2',$_referer);
+	header("data-referer: $_referer");
+	// if(preg_match('/^(localhost|(192|52)(\.\d+){3}|(\w+\.)?tagbum\.com)$/',$_referer)){
+		$options=array(
+			'access_control_allow_origin'=>"http://$_referer",
+			'access_control_allow_credentials'=>true
+		);
+	// }
+	unset($_POST['CROSSDOMAIN'],$_REQUEST['CROSSDOMAIN']);
+
+	$handler = new VideoConvertionApp($options);
 	die();
 }
 
